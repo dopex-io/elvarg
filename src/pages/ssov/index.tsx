fc0df72fa5f1365ff6bb1e38a20ff125e4952691
WitherBlock <witherblock@protonmail.com>
2021-10-29 13:53:30 +0400

    chore: deploy ssov

--- src/pages/ssov/index.tsx
+++ src/pages/ssov/index.tsx
@@ -1,4 +1,5 @@
 import Box from '@material-ui/core/Box';
+import Alert from '@material-ui/lab/Alert';
 
 import Typography from 'components/UI/Typography';
 import AppBar from 'components/AppBar';
@@ -11,7 +12,11 @@ const Ssov = () => {
     <SsovProvider>
       <Box className="bg-black min-h-screen">
         <AppBar active="SSOV" />
-        <Box className="py-32 lg:max-w-5xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0">
+        <Box className="pb-32 pt-20 lg:max-w-5xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0">
+          <Alert severity="warning" className="mb-12">
+            Deposits for the first epoch (1st November to 26th November) open on
+            29th October 8:00 AM UTC till 1st November 8:00 AM UTC
+          </Alert>
           <Box className="text-center mx-auto max-w-xl mb-12">
             <Typography variant="h1" className="mb-1">
               Single Staking Option Vaults
