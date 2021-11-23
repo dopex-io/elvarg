02ad921098b10db3c53b2fb76ae03194d1ebc857
hibou-de-nuit <hasminosmin2@gmail.com>
2021-11-23 15:37:19 +0400

    feat: countdown to exercise countdown to exercise window in ssov home screen

--- src/craPages/ssov/index.tsx
+++ src/craPages/ssov/index.tsx
@@ -1,12 +1,22 @@
+import { useContext } from 'react';
 import Head from 'next/head';
 import Box from '@material-ui/core/Box';
+import Countdown from 'react-countdown';
 
 import Typography from 'components/UI/Typography';
 import AppBar from 'components/AppBar';
 import SsovCard from './components/SsovCard';
 import AutoExerciseBanner from 'components/Banners/AutoExerciseBanner';
 
+import { SsovContext } from 'contexts/Ssov';
+
 const Ssov = () => {
+  const context = useContext(SsovContext);
+
+  const {
+    ssovData: { epochTimes },
+  } = context['dpx'];
+
   return (
     <Box className="bg-black min-h-screen">
       <Head>
@@ -23,6 +33,16 @@ const Ssov = () => {
             Supply option liquidity to an Option Vault. Collect premiums from
             option purchases and earn rewards from farms simultaneously.
           </Typography>
+          <Countdown
+            date={new Date((epochTimes[1] - 3600) * 1000)}
+            renderer={({ days, hours, minutes, seconds }) => {
+              return (
+                <span className="text-wave-blue">
+                  Time left to exercise: {days}d {hours}h {minutes}m {seconds}s
+                </span>
+              );
+            }}
+          />
         </Box>
         <Box className="flex flex-col lg:flex-row lg:space-x-24 space-y-12 lg:space-y-0 justify-center items-center">
           <SsovCard ssov="dpx" />
