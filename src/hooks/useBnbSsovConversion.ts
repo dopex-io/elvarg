cd56a18d6204cd2b83fd244a29f39aee7ff7b508
Aercwarden <arc@dopex.io>
2022-01-18 01:48:07 +0400

    fix: vbnb conversion hook

--- src/hooks/useBnbSsovConversion.ts
+++ src/hooks/useBnbSsovConversion.ts
@@ -1,5 +1,5 @@
 import { WalletContext } from 'contexts/Wallet';
-import { BigNumber, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import { useContext, useEffect, useState } from 'react';
 import oneEBigNumber from 'utils/math/oneEBigNumber';
 
@@ -7,22 +7,24 @@ const useBnbSsovConversion = () => {
   const { contractAddresses, provider } = useContext(WalletContext);
   const [oneBnbtoVbnb, setOneBnbtoVbnb] = useState<String>('0');
   const [oneVbnbtoBnb, setOneVbnbtoBnb] = useState<String>('0');
-  const [abi] = useState([
-    'function bnbToVbnb(uint256 bnbAmount) public view returns (uint256)',
-    ' function vbnbToBnb(uint256 vbnbAmount) public view returns (uint256)',
-  ]);
-  const [contract] = useState(
-    new ethers.Contract(contractAddresses.SSOV.BNB.Vault, abi, provider)
-  );
 
   useEffect(() => {
     if (!contractAddresses.SSOV.BNB || !provider) return;
 
     (async () => {
-      setOneVbnbtoBnb((await contract.vbnbToBnb(oneEBigNumber(8))).toString());
-      setOneBnbtoVbnb((await contract.bnbToVbnb(oneEBigNumber(18))).toString());
+      const abi = [
+        'function bnbToVbnb(uint256 bnbAmount) public view returns (uint256)',
+        ' function vbnbToBnb(uint256 vbnbAmount) public view returns (uint256)',
+      ];
+      const bnbSsov = new ethers.Contract(
+        contractAddresses.SSOV.BNB.Vault,
+        abi,
+        provider
+      );
+      setOneVbnbtoBnb((await bnbSsov.vbnbToBnb(oneEBigNumber(8))).toString());
+      setOneBnbtoVbnb((await bnbSsov.bnbToVbnb(oneEBigNumber(18))).toString());
     })();
-  }, [contractAddresses.SSOV, provider, abi, contract]);
+  }, [contractAddresses.SSOV, provider]);
 
   const convertToBNB = (amount: number) => {
     return (amount * Number(oneVbnbtoBnb)) / 1e26;
@@ -32,11 +34,7 @@ const useBnbSsovConversion = () => {
     return (amount * Number(oneBnbtoVbnb)) / 1e26;
   };
 
-  const convertVbnbToBnbRaw = async (amount: BigNumber) => {
-    return await contract.vbnbToBnb(amount);
-  };
   return {
-    convertVbnbToBnbRaw,
     convertToBNB,
     convertToVBNB,
   };
