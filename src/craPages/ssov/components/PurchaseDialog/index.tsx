04e4e752de5fcf3adbd69ebfe9807233ba6522bb
hellodespereaux <hellodespereaux@gmail.com>
2022-02-08 12:31:10 +0100

    fix: optimized svgs, sorted imports

--- src/craPages/ssov/components/PurchaseDialog/index.tsx
+++ src/craPages/ssov/components/PurchaseDialog/index.tsx
@@ -5,22 +5,49 @@ import React, {
   useMemo,
   useCallback,
 } from 'react';
+
+import {
+  Addresses,
+  ERC20,
+  ERC20__factory,
+  ERC20SSOV1inchRouter__factory,
+  NativeSSOV1inchRouter__factory,
+  Aggregation1inchRouterV4__factory,
+} from '@dopex-io/sdk';
+
 import { useFormik } from 'formik';
 import { utils as ethersUtils, BigNumber, ethers } from 'ethers';
 import * as yup from 'yup';
 import noop from 'lodash/noop';
+import debounce from 'lodash/debounce';
+import format from 'date-fns/format';
+import { useDebounce } from 'use-debounce';
+import axios from 'axios';
+import styles from './styles.module.scss';
+
 import Box from '@material-ui/core/Box';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import Slide from '@material-ui/core/Slide';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import debounce from 'lodash/debounce';
+
 import Dialog from 'components/UI/Dialog';
 import Typography from 'components/UI/Typography';
 import CustomButton from 'components/UI/CustomButton';
 import PnlChart from 'components/PnlChart';
+import ZapIn from '../../../../components/ZapIn';
+import EstimatedGasCostButton from '../../../../components/EstimatedGasCostButton';
+import ZapInButton from '../../../../components/ZapInButton';
+import ZapOutButton from '../../../../components/ZapOutButton';
+import getContractReadableAmount from '../../../../utils/contracts/getContractReadableAmount';
+import getDecimalsFromSymbol from '../../../../utils/general/getDecimalsFromSymbol';
+import useBnbSsovConversion from '../../../../hooks/useBnbSsovConversion';
 import { getValueInUsdFromSymbol } from 'utils/general/getValueInUsdFromSymbol';
+import getUserReadableAmount from 'utils/contracts/getUserReadableAmount';
+import formatAmount from 'utils/general/formatAmount';
 
 import { WalletContext } from 'contexts/Wallet';
 import { AssetsContext, IS_NATIVE } from 'contexts/Assets';
@@ -32,33 +59,12 @@ import {
 } from 'contexts/Ssov';
 
 import useSendTx from 'hooks/useSendTx';
-
-import getUserReadableAmount from 'utils/contracts/getUserReadableAmount';
-import formatAmount from 'utils/general/formatAmount';
-
 import { MAX_VALUE, SSOV_MAP } from 'constants/index';
-import format from 'date-fns/format';
-import Menu from '@material-ui/core/Menu';
-import Slide from '@material-ui/core/Slide';
-import {
-  Addresses,
-  ERC20,
-  ERC20__factory,
-  ERC20SSOV1inchRouter__factory,
-  NativeSSOV1inchRouter__factory,
-  Aggregation1inchRouterV4__factory,
-} from '@dopex-io/sdk';
-import ZapIn from '../../../../components/ZapIn';
-import { useDebounce } from 'use-debounce';
-import axios from 'axios';
-import styles from './styles.module.scss';
-import EstimatedGasCostButton from '../../../../components/EstimatedGasCostButton';
-import ZapInButton from '../../../../components/ZapInButton';
-import ZapOutButton from '../../../../components/ZapOutButton';
-import getContractReadableAmount from '../../../../utils/contracts/getContractReadableAmount';
-import getDecimalsFromSymbol from '../../../../utils/general/getDecimalsFromSymbol';
-import { Add } from '@material-ui/icons';
-import useBnbSsovConversion from '../../../../hooks/useBnbSsovConversion';
+import BigCrossIcon from '../../../../components/Icons/BigCrossIcon';
+import ZapIcon from '../../../../components/Icons/ZapIcon';
+import CircleIcon from '../../../../components/Icons/CircleIcon';
+import AlarmIcon from '../../../../components/Icons/AlarmIcon';
+import ReloadIcon from '../../../../components/Icons/ReloadIcon';
 
 export interface Props {
   open: boolean;
@@ -770,18 +776,7 @@ const PurchaseDialog = ({
           }
           onClick={handleClose}
         >
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M13.3002 0.709727C12.9102 0.319727 12.2802 0.319727 11.8902 0.709727L7.00022 5.58973L2.11022 0.699727C1.72022 0.309727 1.09021 0.309727 0.700215 0.699727C0.310215 1.08973 0.310215 1.71973 0.700215 2.10973L5.59022 6.99973L0.700215 11.8897C0.310215 12.2797 0.310215 12.9097 0.700215 13.2997C1.09021 13.6897 1.72022 13.6897 2.11022 13.2997L7.00022 8.40973L11.8902 13.2997C12.2802 13.6897 12.9102 13.6897 13.3002 13.2997C13.6902 12.9097 13.6902 12.2797 13.3002 11.8897L8.41021 6.99973L13.3002 2.10973C13.6802 1.72973 13.6802 1.08973 13.3002 0.709727Z"
-              fill="#3E3E3E"
-            />
-          </svg>
+          <BigCrossIcon className="" />
         </IconButton>
       </Box>
 
@@ -838,38 +833,7 @@ const PurchaseDialog = ({
                 {isZapActive && <span>{tokenName} </span>}
               </span>
             </Typography>
-            {isZapActive && (
-              <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="mt-3 ml-1"
-              >
-                <path
-                  d="M6.00001 0.178345C2.78584 0.178345 0.178345 2.78584 0.178345 6.00001C0.178345 9.21418 2.78584 11.8217 6.00001 11.8217C9.21418 11.8217 11.8217 9.21418 11.8217 6.00001C11.8217 2.78584 9.21418 0.178345 6.00001 0.178345Z"
-                  fill="url(#paint0_linear_1600_23889)"
-                />
-                <path
-                  d="M4.02883 8.76422L5.23633 6.67277L3.89254 5.89694C3.70563 5.78902 3.696 5.51403 3.88318 5.39982L7.8335 2.85101C8.08677 2.68065 8.39587 2.97362 8.24129 3.24136L7.0192 5.35807L8.30236 6.09891C8.48928 6.20682 8.49677 6.47384 8.31969 6.59389L4.44458 9.15245C4.18334 9.32493 3.87424 9.03197 4.02883 8.76422Z"
-                  fill="white"
-                />
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_1600_23889"
-                    x1="11.8217"
-                    y1="13.485"
-                    x2="0.0889196"
-                    y2="0.257665"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#002EFF" />
-                    <stop offset="1" stopColor="#22E1FF" />
-                  </linearGradient>
-                </defs>
-              </svg>
-            )}
+            {isZapActive && <ZapIcon className="mt-3 ml-1" id="5" />}
           </Box>
           <Box className="ml-auto mr-0">
             <Typography
@@ -1049,26 +1013,23 @@ const PurchaseDialog = ({
       </Box>
 
       <Box className="flex mt-5 mb-5">
-        <svg
+        <CircleIcon
           className={
             isChartVisible
               ? 'ml-auto mr-3 h-5 w-5 fill-gray-800 stroke-gray-100 opacity-10 cursor-pointer'
               : 'ml-auto mr-3 h-5 w-5 fill-white stroke-white cursor-pointer'
           }
           onClick={() => setIsChartVisible(false)}
-        >
-          <circle cx="5" cy="5" r="4" />
-        </svg>
-        <svg
+        />
+
+        <CircleIcon
           className={
             isChartVisible
               ? 'mr-auto ml-0 h-5 w-5 fill-white stroke-white cursor-pointer'
               : 'mr-auto ml-0 h-5 w-5 fill-gray-800 stroke-gray-100 opacity-10 cursor-pointer'
           }
           onClick={() => setIsChartVisible(true)}
-        >
-          <circle cx="5" cy="5" r="4" />
-        </svg>
+        />
       </Box>
 
       <Box className="rounded-xl p-4 border border-neutral-800 w-full bg-umbra">
@@ -1156,18 +1117,7 @@ const PurchaseDialog = ({
 
         <Box className="flex">
           <Box className="flex text-center p-2 mr-2 mt-1">
-            <svg
-              width="18"
-              height="19"
-              viewBox="0 0 18 19"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.16667 8.0833H9.5775L6.1675 12.0708C5.66333 12.6666 6.085 13.5833 6.86417 13.5833H10.8333C11.3375 13.5833 11.75 13.1708 11.75 12.6666C11.75 12.1625 11.3375 11.75 10.8333 11.75H8.4225L11.8325 7.76247C12.3367 7.16663 11.915 6.24997 11.1358 6.24997H7.16667C6.6625 6.24997 6.25 6.66247 6.25 7.16663C6.25 7.6708 6.6625 8.0833 7.16667 8.0833ZM17.525 3.88497C17.2042 4.26997 16.6267 4.32497 16.2325 4.00413L13.4183 1.65747C13.0333 1.32747 12.9783 0.749966 13.3083 0.364966C13.6292 -0.0200341 14.2067 -0.075034 14.6008 0.245799L17.415 2.59247C17.8 2.92247 17.855 3.49997 17.525 3.88497ZM0.475 3.88497C0.795834 4.27913 1.37333 4.32497 1.75833 4.00413L4.5725 1.65747C4.96667 1.32747 5.02167 0.749966 4.69167 0.364966C4.37083 -0.0292008 3.79333 -0.075034 3.40833 0.245799L0.585 2.59247C0.2 2.92247 0.145 3.49997 0.475 3.88497ZM9 3.49997C12.5383 3.49997 15.4167 6.3783 15.4167 9.91663C15.4167 13.455 12.5383 16.3333 9 16.3333C5.46167 16.3333 2.58333 13.455 2.58333 9.91663C2.58333 6.3783 5.46167 3.49997 9 3.49997ZM9 1.66663C4.44417 1.66663 0.75 5.3608 0.75 9.91663C0.75 14.4725 4.44417 18.1666 9 18.1666C13.5558 18.1666 17.25 14.4725 17.25 9.91663C17.25 5.3608 13.5558 1.66663 9 1.66663Z"
-                fill="#6DFFB9"
-              />
-            </svg>
+            <AlarmIcon />
           </Box>
           <Typography variant="h6" className="text-stieglitz">
             This option will <span className="text-white">Auto Exercise</span>{' '}
@@ -1204,18 +1154,7 @@ const PurchaseDialog = ({
           {formik.values.optionsAmount > 0 ? (
             isFetchingPath ? (
               <Box className={'flex'}>
-                <svg
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.7081 2.29182C10.3497 0.93349 8.42473 0.150156 6.30807 0.366823C3.24973 0.675156 0.733066 3.15849 0.3914 6.21682C-0.0669337 10.2585 3.05807 13.6668 6.99973 13.6668C9.65807 13.6668 11.9414 12.1085 13.0081 9.86682C13.2747 9.30849 12.8747 8.66682 12.2581 8.66682C11.9497 8.66682 11.6581 8.83349 11.5247 9.10849C10.5831 11.1335 8.32473 12.4168 5.85807 11.8668C4.00807 11.4585 2.5164 9.95016 2.12473 8.10016C1.42473 4.86682 3.88307 2.00016 6.99973 2.00016C8.38307 2.00016 9.6164 2.57516 10.5164 3.48349L9.25807 4.74182C8.73307 5.26682 9.09973 6.16682 9.8414 6.16682H12.8331C13.2914 6.16682 13.6664 5.79182 13.6664 5.33349V2.34182C13.6664 1.60016 12.7664 1.22516 12.2414 1.75016L11.7081 2.29182Z"
-                    fill="#8E8E8E"
-                  />
-                </svg>
+                <ReloadIcon className="" />
                 <span className="ml-2">Elaborating best route...</span>
               </Box>
             ) : getUserReadableAmount(state.totalCost, 18) > purchasePower ? (
