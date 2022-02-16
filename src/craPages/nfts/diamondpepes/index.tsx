1e41c7b1c64943111d47e7879bf206785b531bb5
hellodespereaux <hellodespereaux@gmail.com>
2022-02-16 11:49:35 +0100

    feat: implemented context for diamondpepe

--- src/craPages/nfts/diamondpepes/index.tsx
+++ src/craPages/nfts/diamondpepes/index.tsx
@@ -4,10 +4,17 @@ import Box from '@material-ui/core/Box';
 import Typography from 'components/UI/Typography';
 import AppBar from 'components/AppBar';
 import styles from './styles.module.scss';
-import CustomButton from '../../../components/UI/CustomButton';
 import { Tooltip } from '@material-ui/core';
+import { useContext, useEffect, useState } from 'react';
+import { DiamondPepeContext } from '../../../contexts/DiamondPepe';
+import { DiamondPepeProvider } from '../../../contexts/DiamondPepe';
 
 const DiamondPepesNfts = () => {
+  const { data, userData } = useContext(DiamondPepeContext);
+
+  console.log(data);
+  console.log(userData);
+
   const boxes = [
     { title: 'Genesis', subTitle: 'Collection' },
     { title: '2PM 2/2/22', subTitle: 'Start CET' },
@@ -17,153 +24,156 @@ const DiamondPepesNfts = () => {
   ];
 
   return (
-    <Box className="bg-black min-h-screen">
-      <Head>
-        <title>Diamond Pepes NFTs | Dopex</title>
-      </Head>
-      <Box>
-        <Box className={styles.backgroundOverlay} />
-        <Box className={styles.mobileBackgroundOverlay} />
-        <AppBar />
-        <Box className="pt-28 md:pt-32 pb-32 lg:max-w-9xl md:max-w-7xl sm:max-w-xl mx-auto px-4 lg:px-0">
-          <Box className="text-center mx-auto md:mb-12 lg:mt-24 flex">
-            <img
-              src={'/assets/diamondpepes.svg'}
-              className="ml-auto mr-auto z-1 relative md:w-auto w-60"
-            />
-          </Box>
-          <Box className="mt-6 md:mt-2 max-w-4xl mx-auto">
-            <Typography
-              variant="h3"
-              className="text-[#78859E] text-center leading-7 md:leading-10 z-40 relative font-['Minecraft'] text-[1rem]"
-            >
-              2,222 Unique Diamond Pepes up for grabs. Free mint passes by
-              staking LP Tokens. Zap In with any asset.
-            </Typography>
-          </Box>
-          <Box className="pl-4 pr-4 md:flex border border-[#232935] w-full mt-9 bg-[#181C24] z-1 relative">
-            {boxes.map((box, i) => (
-              <Box
-                className={
-                  i === boxes.length - 1
-                    ? 'md:w-1/5 w-1/2 border-b md:border-b-0 border-neutral-800'
-                    : 'md:w-1/5 w-1/2 border-b md:border-b-0 md:border-r border-neutral-800 md:mr-4'
-                }
+    <DiamondPepeProvider>
+      <Box className="bg-black min-h-screen">
+        <Head>
+          <title>Diamond Pepes NFTs | Dopex</title>
+        </Head>
+        <Box>
+          <Box className={styles.backgroundOverlay} />
+          <Box className={styles.mobileBackgroundOverlay} />
+          <AppBar />
+          <Box className="pt-28 md:pt-32 pb-32 lg:max-w-9xl md:max-w-7xl sm:max-w-xl mx-auto px-4 lg:px-0">
+            <Box className="text-center mx-auto md:mb-12 lg:mt-24 flex">
+              <img
+                src={'/assets/diamondpepes.svg'}
+                className="ml-auto mr-auto z-1 relative md:w-auto w-60"
+              />
+            </Box>
+            <Box className="mt-6 md:mt-2 max-w-4xl mx-auto">
+              <Typography
+                variant="h3"
+                className="text-[#78859E] text-center leading-7 md:leading-10 z-40 relative font-['Minecraft'] text-[1rem]"
               >
-                <Typography
-                  variant={'h3'}
-                  className="text-white text-base pt-4 pb-1 font-['Minecraft']"
+                2,222 Unique Diamond Pepes up for grabs. Free mint passes by
+                staking LP Tokens. Zap In with any asset.
+              </Typography>
+            </Box>
+            <Box className="pl-4 pr-4 md:flex border border-[#232935] w-full mt-9 bg-[#181C24] z-1 relative">
+              {boxes.map((box, i) => (
+                <Box
+                  key={i}
+                  className={
+                    i === boxes.length - 1
+                      ? 'md:w-1/5 w-1/2 border-b md:border-b-0 border-neutral-800'
+                      : 'md:w-1/5 w-1/2 border-b md:border-b-0 md:border-r border-neutral-800 md:mr-4'
+                  }
                 >
-                  {box['title']}
+                  <Typography
+                    variant={'h3'}
+                    className="text-white text-base pt-4 pb-1 font-['Minecraft']"
+                  >
+                    {box['title']}
+                  </Typography>
+                  <Typography
+                    variant={'h4'}
+                    className="text-[#78859E] pb-3 font-['Minecraft']"
+                  >
+                    {box['subTitle']}
+                  </Typography>
+                </Box>
+              ))}
+            </Box>
+
+            <Box className="p-2 mt-7 md:flex">
+              <Box className="md:w-1/3 p-4 text-center">
+                <Typography
+                  variant="h3"
+                  className="text-white font-display font-['Minecraft'] relative z-1"
+                >
+                  <span className={styles.pepeText}>Deposit LP Tokens</span>
                 </Typography>
                 <Typography
-                  variant={'h4'}
-                  className="text-[#78859E] pb-3 font-['Minecraft']"
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
                 >
-                  {box['subTitle']}
+                  This mint experience only requires you to stake LP tokens to
+                  get your Diamond Pepe(s).
                 </Typography>
+                <Typography
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
+                >
+                  Please note that you'll need to deposit 1.5 LP tokens minimum
+                  per pepe for a guaranteed mint.
+                  <br />
+                  <br />
+                </Typography>
+
+                <Box className="ml-5 mb-5 md:mt-10 md:mb-0">
+                  <Tooltip title={'Not open yet'}>
+                    <button className={styles.pepeButton}>Deposit</button>
+                  </Tooltip>
+                </Box>
               </Box>
-            ))}
-          </Box>
+              <Box className="md:w-1/3 p-4 text-center">
+                <Typography
+                  variant="h3"
+                  className="text-white font-display font-['Minecraft'] relative z-1"
+                >
+                  <span className={styles.pepeText}>Wait for mint</span>
+                </Typography>
+                <Typography
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
+                >
+                  You can mint your NFT when the deposit period ends. This is
+                  set for two days from opening pool.
+                </Typography>
+                <Typography
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
+                >
+                  You can check the reveal of these NFTs on mint day on
+                  tofunft.com. <br />
+                  <br />
+                  Good luck kid.
+                </Typography>
 
-          <Box className="p-2 mt-7 md:flex">
-            <Box className="md:w-1/3 p-4 text-center">
-              <Typography
-                variant="h3"
-                className="text-white font-display font-['Minecraft'] relative z-1"
-              >
-                <span className={styles.pepeText}>Deposit LP Tokens</span>
-              </Typography>
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
-              >
-                This mint experience only requires you to stake LP tokens to get
-                your Diamond Pepe(s).
-              </Typography>
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
-              >
-                Please note that you'll need to deposit 1.5 LP tokens minimum
-                per pepe for a guaranteed mint.
-                <br />
-                <br />
-              </Typography>
-
-              <Box className="ml-5 mb-5 md:mt-10 md:mb-0">
-                <Tooltip title={'Not open yet'}>
-                  <button className={styles.pepeButton}>Deposit</button>
-                </Tooltip>
+                <Box className="ml-5 mb-5 mt-6 md:mt-10 md:mb-0">
+                  <Tooltip title={'Not open yet'}>
+                    <button className={styles.pepeButton} disabled>
+                      2/4/2022
+                    </button>
+                  </Tooltip>
+                </Box>
               </Box>
-            </Box>
-            <Box className="md:w-1/3 p-4 text-center">
-              <Typography
-                variant="h3"
-                className="text-white font-display font-['Minecraft'] relative z-1"
-              >
-                <span className={styles.pepeText}>Wait for mint</span>
-              </Typography>
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
-              >
-                You can mint your NFT when the deposit period ends. This is set
-                for two days from opening pool.
-              </Typography>
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
-              >
-                You can check the reveal of these NFTs on mint day on
-                tofunft.com. <br />
-                <br />
-                Good luck kid.
-              </Typography>
+              <Box className="md:w-1/3 p-4 text-center">
+                <Typography
+                  variant="h3"
+                  className="text-white font-display font-['Minecraft'] relative z-1"
+                >
+                  <span className={styles.pepeText}> Withdraw Later</span>
+                </Typography>
 
-              <Box className="ml-5 mb-5 mt-6 md:mt-10 md:mb-0">
-                <Tooltip title={'Not open yet'}>
-                  <button className={styles.pepeButton} disabled>
-                    2/4/2022
-                  </button>
-                </Tooltip>
-              </Box>
-            </Box>
-            <Box className="md:w-1/3 p-4 text-center">
-              <Typography
-                variant="h3"
-                className="text-white font-display font-['Minecraft'] relative z-1"
-              >
-                <span className={styles.pepeText}> Withdraw Later</span>
-              </Typography>
+                <Typography
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
+                >
+                  Your stake is locked for 14 days. Once unlocked you can
+                  withdraw all funds anytime.
+                </Typography>
+                <Typography
+                  variant="h4"
+                  className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
+                >
+                  Excess deposits that doesn't end up with additional pepes will
+                  also return its share of the farming reward upon withdrawing.
+                </Typography>
 
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-4"
-              >
-                Your stake is locked for 14 days. Once unlocked you can withdraw
-                all funds anytime.
-              </Typography>
-              <Typography
-                variant="h4"
-                className="text-[#78859E] font-['Minecraft'] relative z-1 mt-5"
-              >
-                Excess deposits that doesn't end up with additional pepes will
-                also return its share of the farming reward upon withdrawing.
-              </Typography>
-
-              <Box className="ml-5 mb-5 mt-6 md:mt-10 md:mb-0">
-                <Tooltip title={'Not open yet'}>
-                  <button className={styles.pepeButton} disabled>
-                    3/3/2022
-                  </button>
-                </Tooltip>
+                <Box className="ml-5 mb-5 mt-6 md:mt-10 md:mb-0">
+                  <Tooltip title={'Not open yet'}>
+                    <button className={styles.pepeButton} disabled>
+                      3/3/2022
+                    </button>
+                  </Tooltip>
+                </Box>
               </Box>
             </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </DiamondPepeProvider>
   );
 };
 
