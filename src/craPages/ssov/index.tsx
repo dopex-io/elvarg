af66eb5c3d32ac6b3780c6602d52a5a3cccd4e32
hibou-de-nuit <hp@dopex.io>
2022-02-28 22:38:16 +0400

    fix: duplicate ssov card

--- src/craPages/ssov/index.tsx
+++ src/craPages/ssov/index.tsx
@@ -40,7 +40,7 @@ const Ssov = () => {
   const keys = useMemo(() => {
     if (!ssovs) return [];
     else if (chainId === 56) return [56, 42161, 43114];
-    else if (chainId === 43114) return [43114, 42161, 43114];
+    else if (chainId === 43114) return [43114, 42161];
     else return [42161, 56, 43114];
   }, [ssovs, chainId]);
 
