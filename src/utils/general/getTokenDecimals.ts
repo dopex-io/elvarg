03e05ed3b30932cd6fab9492cd7ef7cc644f22b3
hellodespereaux <hellodespereaux@gmail.com>
2022-03-01 17:01:33 +0100

    fix: it's safer to use only uppercase symbols

--- src/utils/general/getTokenDecimals.ts
+++ src/utils/general/getTokenDecimals.ts
@@ -1,7 +1,7 @@
 import { TOKEN_DECIMALS } from 'constants/index';
 
 const getTokenDecimals = (tokenSymbol: string) => {
-  return TOKEN_DECIMALS[tokenSymbol] || 18;
+  return TOKEN_DECIMALS[tokenSymbol.toLocaleUpperCase()] || 18;
 };
 
 export default getTokenDecimals;
