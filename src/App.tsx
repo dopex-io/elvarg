7c3a7afcab862337beb3e1fb56a299460f1d2ed6
Despereaux <96305652+hellodespereaux@users.noreply.github.com>
2022-03-10 16:02:46 +0100

    feat: implemented tzwap feature (#124)
    * feat: implementing tzwap
    
    * feat: first page of UI is ready
    
    * feat: implemented logic for UI elements
    
    * feat: added logic
    
    * feat: added logic
    
    * fix: use WETH address instead of eth for dstToken
    
    * fix: minor fixs and package json update
    
    * feat: implemented active orders section, updated fees
    
    * feat: finished implementation
    
    * feat: added alts
    
    * fix: orders
    
    * fix: adjusted calcs & refactoring
    
    * fix: refactoring
    
    * fix: hardcoded new address while sdk is not published
    
    * fix: updated min fees
    
    * fix: adjusted fee logic
    
    * fix: adjusted fee logic
    
    * fix: reduced min fees
    
    * feat: added token to exclude
    
    * fix: tick size should be at least 100$
    
    * fix: updated hardcoded address
    
    * fix rules
    
    * fix: rules
    
    * feat: udpated UI
    
    * fix: adjusted fees
    
    * fix: adjusted stats
    
    * fix: see only yours
    
    * fix: tzwapRouter
    
    * feat: added fetching spin
    
    * fix: update fetching orders
    
    * feat: split in multiple components
    
    * fix: use batch size instead of tick size
    
    * feat: added disclaimer
    
    * feat: tzwap feature (#120)
    
    * feat: implementing tzwap
    
    * feat: first page of UI is ready
    
    * feat: implemented logic for UI elements
    
    * feat: added logic
    
    * fix: use WETH address instead of eth for dstToken
    
    * fix: minor fixs and package json update
    
    * feat: implemented active orders section, updated fees
    
    * feat: finished implementation
    
    * feat: added alts
    
    * fix: orders
    
    * fix: adjusted calcs & refactoring
    
    * fix: refactoring
    
    * fix: hardcoded new address while sdk is not published
    
    * fix: updated min fees
    
    * fix: adjusted fee logic
    
    * feat: added token to exclude
    
    * fix: tick size should be at least 100$
    
    * fix: updated hardcoded address
    
    * fix rules
    
    * feat: udpated UI
    
    * fix: adjusted fees
    
    * fix: adjusted stats
    
    * fix: tzwapRouter
    
    * feat: added fetching spin
    
    * fix: update fetching orders
    
    * feat: split in multiple components
    
    * fix: use batch size instead of tick size
    
    * feat: added disclaimer
    
    Co-authored-by: WitherBlock <witherblock@protonmail.com>
    
    * fix: adjusted ts issue in TokenSelector
    
    * fix: adjusted total calculation
    
    * revert zapin changes
    
    * fix: refactoring & merge with new changes of develop branch
    
    * feat: added tooltip for batch size
    
    * chore: updated sdk, removed hardcoded address
    
    Co-authored-by: WitherBlock <witherblock@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ const PledgeDiamondPepesNfts = lazy(
   () => import('craPages/nfts/diamondpepes/pledge')
 );
 const Oracles = lazy(() => import('craPages/oracles'));
+const Tzwap = lazy(() => import('craPages/tzwap'));
 // const Portfolio = lazy(() => import('pages/portfolio'));
 // const Options = lazy(() => import('pages/options'));
 // const Pools = lazy(() => import('pages/pools'));
@@ -88,6 +89,7 @@ const NftsRoutes = () => {
     </NftsProvider>
   );
 };
+
 function AppRoutes() {
   // if (BUILD === 'testnet') {
   //   return (
@@ -119,6 +121,7 @@ function AppRoutes() {
               <Route path="farms/*" element={<FarmRoutes />} />
               <Route path="nfts/*" element={<NftsRoutes />} />
               <Route path="oracles" element={<Oracles />} />
+              <Route path="tzwap" element={<Tzwap />} />
               <Route path="*" element={<Error statusCode={404} />} />
             </Routes>
             <ChangeNetworkDialog />
