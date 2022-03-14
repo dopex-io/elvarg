5361ae1aeadeb05607c11d7e9a6922074171b268
hellodespereaux <hellodespereaux@gmail.com>
2022-03-14 15:26:21 +0100

    fix: added alt

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -288,6 +288,7 @@ export default function AppBar(props: AppBarProps) {
                     <img
                       src="/assets/imtoken.png"
                       className="w-3 h-3 mr-2 mt-0.5"
+                      alt="ImToken"
                     />
                   ) : null}
                   {walletButtonContent}
