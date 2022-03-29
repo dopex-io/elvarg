bf9a4663fd07aa84c8102ef14f62e3022ea8f858
hellodespereaux <hellodespereaux@gmail.com>
2022-03-30 00:03:04 +0200

    fix: apy overflow

--- src/utils/general/formatAmount.ts
+++ src/utils/general/formatAmount.ts
@@ -8,9 +8,9 @@ function formatAmount(
   if (showDash && typecastedAmount === 0) return '--';
   if (showK && typecastedAmount > 999 && typecastedAmount < 1000000) {
     return (typecastedAmount / 1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million
-  } else if (typecastedAmount > 1000000 && typecastedAmount < 100000000000) {
+  } else if (typecastedAmount >= 1000000 && typecastedAmount < 100000000000) {
     return (typecastedAmount / 1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million
-  } else if (typecastedAmount > 100000000000) {
+  } else if (typecastedAmount >= 100000000000) {
     return '∞';
   }
   return Number(typecastedAmount.toFixed(decimalPoints)).toLocaleString(); // if value < 1000, nothing to do
