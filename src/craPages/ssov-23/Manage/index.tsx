3bd204b152f6c2a3e66af7948532d50ecfef4a78
WitherBlock <witherblock@protonmail.com>
2022-04-12 21:14:27 +0400

    just something we do

---
+++ src/craPages/ssov-23/Manage/index.tsx
@@ -0,0 +1,80 @@
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Head from 'next/head';
+import Box from '@mui/material/Box';
+
+import AppBar from 'components/AppBar';
+import Description from '../components/Description';
+import ManageCard from '../components/ManageCard';
+import ExerciseList from '../components/ExerciseList';
+import Stats from '../components/Stats';
+import PageLoader from 'components/PageLoader';
+import EmergencyNoticeBanner from 'components/Banners/EmergencyNoticeBanner';
+
+import { SsovContext, SsovProvider } from 'contexts/Ssov';
+
+const Manage = () => {
+  const { asset, type } = useParams();
+  const {
+    ssovData,
+    ssovEpochData,
+    ssovUserData,
+    setSelectedSsov,
+    selectedSsov,
+  } = useContext(SsovContext);
+
+  useEffect(() => {
+    setSelectedSsov({ token: asset, type: type.toUpperCase() });
+  }, [setSelectedSsov, asset, type]);
+
+  if (ssovData === undefined || ssovEpochData === undefined)
+    return (
+      <Box className="overflow-x-hidden bg-black h-screen">
+        <PageLoader />
+      </Box>
+    );
+
+  return (
+    <Box className="overflow-x-hidden bg-black h-screen">
+      <Head>
+        <title>SSOV | Dopex</title>
+      </Head>
+      <AppBar active="SSOV" />
+      <Box className="py-12 lg:max-w-5xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0">
+        <Box className="flex flex-col mt-20">
+          <Box className="flex md:flex-row flex-col mb-4 md:justify-between items-center md:items-start">
+            <Description
+              ssovData={ssovData}
+              ssovEpochData={ssovEpochData}
+              ssovUserData={ssovUserData}
+              type={selectedSsov.type}
+            />
+            <ManageCard />
+          </Box>
+          {ssovUserData === undefined ? null : <ExerciseList />}
+          {selectedSsov.type === 'PUT' ? null : <Stats className="mt-4" />}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const ManagePage = () => {
+  const { asset } = useParams();
+  return (
+    <SsovProvider>
+      {asset === 'METIS' && (
+        <EmergencyNoticeBanner
+          paragraph={
+            'All METIS deposits have been safely withdrawn and will be dispersed to the depositors soon.'
+          }
+          title={'Emergency Withdrawal Notice'}
+        />
+      )}
+
+      <Manage />
+    </SsovProvider>
+  );
+};
+
+export default ManagePage;
---
+++ src/craPages/ssov-23/components/SsovCard/index.tsx
@@ -0,0 +1,127 @@
+import { useContext, useMemo } from 'react';
+import cx from 'classnames';
+import Box from '@mui/material/Box';
+
+import { BnbConversionContext } from 'contexts/BnbConversion';
+
+import CustomButton from 'components/UI/CustomButton';
+import Typography from 'components/UI/Typography';
+import InfoBox from '../InfoBox';
+
+import Coin from 'assets/icons/Coin';
+import Action from 'assets/icons/Action';
+
+import formatAmount from 'utils/general/formatAmount';
+import getUserReadableAmount from 'utils/contracts/getUserReadableAmount';
+
+import { SSOV_MAP } from 'constants/index';
+import ssovInfo from 'constants/ssovInfo';
+
+import styles from './styles.module.scss';
+import { BigNumber } from 'ethers';
+
+function SsovCard(props) {
+  const { className, data } = props;
+  const { convertToBNB } = useContext(BnbConversionContext);
+
+  const {
+    currentEpoch,
+    totalEpochDeposits,
+    apy,
+    tvl,
+    name,
+    type,
+    underlyingDecimals,
+  } = data;
+
+  const info = useMemo(() => {
+    if (!convertToBNB) return [];
+    return [
+      {
+        heading: 'APY',
+        value: `${apy === 0 ? '...' : `${formatAmount(apy, 0, true)}%`}`,
+        Icon: Action,
+        tooltip:
+          type === 'put'
+            ? 'This is the base APY calculated from Curve 2Pool Fees and Rewards'
+            : ssovInfo[name].aprToolTipMessage,
+      },
+      {
+        heading: 'TVL',
+        value: tvl === 0 ? '...' : formatAmount(tvl, 0, true),
+        Icon: Coin,
+      },
+      {
+        heading: 'DEPOSITS',
+        value: `${formatAmount(
+          name === 'BNB'
+            ? convertToBNB(BigNumber.from(totalEpochDeposits)).toString()
+            : getUserReadableAmount(totalEpochDeposits, underlyingDecimals),
+          0,
+          true
+        )}`,
+        imgSrc: type === 'put' ? '/assets/2crv.png' : SSOV_MAP[name].imageSrc,
+      },
+    ];
+  }, [
+    apy,
+    convertToBNB,
+    name,
+    totalEpochDeposits,
+    tvl,
+    type,
+    underlyingDecimals,
+  ]);
+
+  return (
+    <Box className={cx('p-[1px] rounded-xl', styles[name], styles.Box)}>
+      <Box
+        className={cx(
+          'flex flex-col bg-cod-gray p-4 rounded-xl h-full mx-auto',
+          className
+        )}
+      >
+        <Box>
+          <Box className="flex flex-row mb-4">
+            <Box className="mr-4 h-8 max-w-14 flex flex-row">
+              <img
+                className="w-9 h-9"
+                src={SSOV_MAP[name].imageSrc}
+                alt={name}
+              />
+            </Box>
+            <Box className="flex flex-grow items-center justify-between">
+              <Typography variant="h4" className="mr-2 font-bold">
+                {name}
+              </Typography>
+              <img
+                src={'/assets/' + type + 's.svg'}
+                className="w-12"
+                alt={type}
+              />
+            </Box>
+          </Box>
+          <Box className="grid grid-cols-3 gap-2 mb-2">
+            {info.map((item) => {
+              return <InfoBox key={item.heading} {...item} />;
+            })}
+          </Box>
+
+          <CustomButton
+            size="medium"
+            className="my-4"
+            href={`/ssov/${type}/${name}`}
+            fullWidth
+          >
+            Manage
+          </CustomButton>
+          <Typography variant="h6" className="text-stieglitz">
+            Epoch {currentEpoch}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default SsovCard;
