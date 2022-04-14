5e6098b692eb413ca3bf2a42eb458ce7a8b3ca0a
WitherBlock <witherblock@protonmail.com>
2022-04-14 23:16:07 +0400

    fix: ssov homepage

--- src/craPages/ssov/index.tsx
+++ src/craPages/ssov/index.tsx
@@ -49,9 +49,8 @@ const Ssov = () => {
   const keys = useMemo(() => {
     if (!ssovs) return [];
     else if (chainId === 56) return [56, 42161, 43114];
-    else if (chainId === 43114) return [43114, 42161];
-    else if (chainId === 1088) return [1088];
-    else return [42161, 56, 43114, 1088];
+    else if (chainId === 43114) return [43114, 42161, 56];
+    else return [42161, 56, 43114];
   }, [ssovs, chainId]);
 
   const ssovsAssets = useMemo(() => {
