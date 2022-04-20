bc013f9d1f3fd5fa944a55ee21a1de11d4119d35
hellodespereaux <hellodespereaux@gmail.com>
2022-04-20 15:22:26 +0200

    fix: vaults

--- src/craPages/ssov-v3/Manage/index.tsx
+++ src/craPages/ssov-v3/Manage/index.tsx
@@ -28,7 +28,7 @@ const Manage = () => {
       <Head>
         <title>SSOV | Dopex</title>
       </Head>
-      <AppBar active="SSOV" />
+      <AppBar active="vaults" />
       <Box className="py-12 lg:max-w-5xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0">
         <Box className="flex flex-col mt-20">
           <Box className="flex md:flex-row flex-col mb-4 md:justify-between items-center md:items-start">
