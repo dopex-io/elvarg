ef070ca0c919e8a5e18c6fb7d0c878b5a130b61e
hellodespereaux <hellodespereaux@gmail.com>
2022-05-02 21:37:08 +0200

    fix: use ankr

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -320,7 +320,7 @@ export const TOKENS = Object.keys(TOKEN_DATA);
 export const CHAIN_ID_TO_RPC = {
   '1': `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
   '56': `https://rpc.ankr.com/bsc/${ANKR_KEY}`,
-  '42161': `https://arbitrum-mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+  '42161': `https://rpc.ankr.com/arbitrum/${ANKR_KEY}`,
   '43114': `https://rpc.ankr.com/avalanche/${ANKR_KEY}`,
   '421611': `https://arbitrum-rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
   '1088': 'https://andromeda.metis.io/?owner=1088',
