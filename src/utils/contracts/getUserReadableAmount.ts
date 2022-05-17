fd87183219bedd836f8424ee6a83e5e97b6a0787
WitherBlock <witherblock@protonmail.com>
2022-05-17 23:25:00 +0400

    fix: farm stake page

--- src/utils/contracts/getUserReadableAmount.ts
+++ src/utils/contracts/getUserReadableAmount.ts
@@ -5,7 +5,7 @@ export default function getUserReadableAmount(
   amount: string | number | BigNumber | ethersBigNumber,
   decimals: string | number = 18
 ): number {
-  if (amount === undefined) return 0;
+  if (amount === undefined || amount === null) return 0;
   return new BigNumber(amount.toString())
     .dividedBy(new BigNumber(10).pow(decimals))
     .toNumber();
