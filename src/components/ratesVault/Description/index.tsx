ddc497fb4695906abef2747a83f7d210f1bbf96b
WitherBlock <witherblock@protonmail.com>
2022-06-14 01:33:31 +0400

    chore: add pusd svg

--- src/components/ratesVault/Description/index.tsx
+++ src/components/ratesVault/Description/index.tsx
@@ -7,7 +7,7 @@ const Description = () => {
     <Box className={'lg:w-3/4'}>
       <Box className={'flex'}>
         <Box className={'rounded-full mt-auto mb-auto'}>
-          {/* <img src="/images/tokens/mim.svg" className="w-[6rem]" alt="ir" /> */}
+          <img src="/images/tokens/pusd.svg" className="w-[6rem]" alt="ir" />
         </Box>
         <Typography
           variant="h3"
