425eb113e8e05e9d0f284c7931171d4cc19c36ba
hellodespereaux <hellodespereaux@gmail.com>
2022-06-22 23:53:17 +0200

    feat: duel pepe implementation

--- src/components/nfts/duel/Duels/index.tsx
+++ src/components/nfts/duel/Duels/index.tsx
@@ -351,7 +351,7 @@ const Duels = () => {
   const duel = useMemo(
     () =>
       new ethers.Contract(
-        '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+        '0x60ebb6174F2c71B2f3d30965A2ffeCa89647c358',
         ABI,
         signer
       ),
