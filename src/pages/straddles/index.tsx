3c2b22447fac8c00dac610f9e158e0d4965865ce
hellodespereaux <hellodespereaux@gmail.com>
2022-07-14 19:24:38 +0200

    fix: fixed ts issues

--- src/pages/straddles/index.tsx
+++ src/pages/straddles/index.tsx
@@ -16,7 +16,7 @@ const Straddles = () => {
         <title>Straddles | Dopex</title>
       </Head>
       <AppBar />
-      <Box className="flex">
+      <Box className="flex pt-5">
         <Box className="w-2/3 ml-5">
           <Box className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
             <TopBar />
@@ -24,27 +24,27 @@ const Straddles = () => {
           <Box className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
             <StatsTab />
           </Box>
-          <Box className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
+          <Box className="pt-8 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
             <Typography variant="h6" className="-ml-1">
               Liquidity
             </Typography>
           </Box>
-          <Box className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0 lg:flex md:flex">
+          <Box className="pt-3 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0 lg:flex md:flex">
             <PoolCard />
             <Box className="mx-2"></Box>
             <TVLCard />
           </Box>
-          <Box className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
+          <Box className="pt-2 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
             <Typography variant="h6" className="-ml-1">
               Deposits
             </Typography>
           </Box>
-          <Box className="h-80 py-4 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0 flex-auto">
+          <Box className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0 flex-auto">
             <DepositsCard />
           </Box>
         </Box>
         <Box className="w-1/3">
-          <Box className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-5 px-2 lg:px-0">
+          <Box className="lg:pt-32 sm:pt-20 pt-20 mx-5 px-2 lg:px-0">
             <DepositBuyCard />
           </Box>
         </Box>
