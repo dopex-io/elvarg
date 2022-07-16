2bb8e06902d6fed8ba2eb7e9fd036443830bea54
WitherBlock <witherblock@protonmail.com>
2022-07-16 09:02:45 +0530

    chore: Revert "fix: disable rdpx purchases (#255)"
    This reverts commit ea6401917957848832a84bd76eb0529a037257f9.

--- src/components/ssov-v3/Description/index.tsx
+++ src/components/ssov-v3/Description/index.tsx
@@ -1,7 +1,6 @@
 import { useMemo, useState, useContext } from 'react';
 import cx from 'classnames';
 import Box from '@mui/material/Box';
-import Tooltip from '@mui/material/Tooltip';
 
 import formatAmount from 'utils/general/formatAmount';
 import getUserReadableAmount from 'utils/contracts/getUserReadableAmount';
@@ -87,25 +86,16 @@ const Description = ({
       <EpochSelector className="mb-6" />
       <Box className="flex justify-center items-center flex-row mb-6">
         <Box className="w-full mr-2">
-          <Tooltip
-            title={
-              ssovData?.underlyingSymbol === 'rDPX'
-                ? 'Purchases will be available soon'
-                : 'Open'
-            }
+          <WalletButton
+            size="medium"
+            fullWidth
+            className="rounded-lg"
+            onClick={() => {
+              accountAddress ? setPurchaseState(true) : connect();
+            }}
           >
-            <WalletButton
-              size="medium"
-              fullWidth
-              className="rounded-lg"
-              onClick={() => {
-                accountAddress ? setPurchaseState(true) : connect();
-              }}
-              disabled={ssovData?.underlyingSymbol === 'rDPX'}
-            >
-              Buy {type} Options
-            </WalletButton>
-          </Tooltip>
+            Buy {type} Options
+          </WalletButton>
         </Box>
       </Box>
       <Box className="grid grid-cols-3 gap-2 mb-6">
