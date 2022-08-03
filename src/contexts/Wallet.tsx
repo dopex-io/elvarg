4e3a8c69fea13fa287536c6ccc5b819c6dba1dab
Aercwarden <arc@dopex.io>
2022-08-03 22:26:57 +0400

    feat: localhost route

--- src/contexts/Wallet.tsx
+++ src/contexts/Wallet.tsx
@@ -68,14 +68,14 @@ const PAGE_TO_SUPPORTED_CHAIN_IDS: {
   '/sale': { default: 1, all: [1] },
   '/oracles': { default: 42161, all: [] },
   '/tzwap': { default: 42161, all: [1, 42161] },
-  '/atlantics': { default: 421611, all: [421611] },
+  '/atlantics': { default: 421611, all: [421611, 1337] },
   '/atlantics/manage/WETH-PUTS-WEEKLY': {
     default: 421611,
-    all: [421611],
+    all: [421611, 1337],
   },
   '/atlantics/manage/WETH-CALLS-WEEKLY': {
     default: 421611,
-    all: [421611],
+    all: [421611, 1337],
   },
   '/ssov-v3/Metis-MONTHLY-CALLS-SSOV-V3': { default: 1088, all: [1088] },
   '/vaults/ir/pool/MIM3CRV-1': { default: 42161, all: [42161] },
