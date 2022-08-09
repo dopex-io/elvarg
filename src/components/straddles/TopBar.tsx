141d773d2f99876ec45b133bd36ce8dd026df7eb
WitherBlock <witherblock@protonmail.com>
2022-08-09 05:20:30 +0400

    feat: add price

--- src/components/straddles/TopBar.tsx
+++ src/components/straddles/TopBar.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+
 import Typography from 'components/UI/Typography';
+import { AssetsContext } from 'contexts/Assets';
 
 const TopBar = () => {
+  const { tokenPrices } = useContext(AssetsContext);
+
+  const ethPrice =
+    tokenPrices.find((token) => token.name === 'ETH')?.price || 0;
+
   return (
     <Box>
       <Box className="flex">
-        <Button size="small" className="-m-4">
+        <Button size="small">
           <ArrowBackIosRoundedIcon className="text-gray-700" />
         </Button>
         <Box sx={{ p: 1 }} className="flex -space-x-4">
@@ -29,6 +36,9 @@ const TopBar = () => {
             ETH
           </Typography>
         </Box>
+        <Typography variant="h4" className="text-wave-blue ml-4">
+          ${ethPrice}
+        </Typography>
       </Box>
     </Box>
   );
