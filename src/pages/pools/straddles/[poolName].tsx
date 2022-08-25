a32b718d3063eb92f7b33877172dedbe119ea50d
hellodespereaux <hellodespereaux@gmail.com>
2022-08-25 18:56:52 +0200

    feat: implemented pools page (only straddles)

--- src/pages/pools/straddles/[poolName].tsx
+++ src/pages/pools/straddles/[poolName].tsx
@@ -28,9 +28,9 @@ const Straddles = ({ poolName }: Props) => {
   return (
     <Box className="bg-black min-h-screen">
       <Head>
-        <title>Straddles | Dopex</title>
+        <title>Straddles {poolName} | Dopex</title>
       </Head>
-      <AppBar active="Straddles" />
+      <AppBar active="Pools" />
       <Box className="md:flex pt-5">
         <Box className="ml-auto lg:w-[45%]">
           <Box className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
