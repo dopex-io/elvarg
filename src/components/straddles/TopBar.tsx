10851c6667a4a7d58e39b91dfcc46db75134623d
hibou-de-nuit <hp@dopex.io>
2022-09-19 21:34:19 +0400

    feat: epoch expired label in straddles manage page

--- src/components/straddles/TopBar.tsx
+++ src/components/straddles/TopBar.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+import { BigNumber } from 'ethers';
 import Box from '@mui/material/Box';
 
 import Typography from 'components/UI/Typography';
@@ -5,13 +7,20 @@ import Typography from 'components/UI/Typography';
 import { useBoundStore } from 'store';
 
 const TopBar = () => {
-  const { tokenPrices, selectedPoolName } = useBoundStore();
+  const { tokenPrices, selectedPoolName, straddlesEpochData } = useBoundStore();
 
   const tokenPrice =
     tokenPrices.find((token) => token.name === selectedPoolName)?.price || 0;
 
+  const isEpochExpired = useMemo(() => {
+    if (!straddlesEpochData) return;
+    return straddlesEpochData?.expiry.lt(
+      BigNumber.from((new Date().getTime() / 1000).toFixed())
+    );
+  }, [straddlesEpochData]);
+
   return (
-    <Box>
+    <Box className="flex justify-between">
       <Box className="flex items-center">
         <Typography
           variant="h5"
@@ -41,6 +50,13 @@ const TopBar = () => {
           ${tokenPrice}
         </Typography>
       </Box>
+      {isEpochExpired ? (
+        <Box className="p-2 my-2 rounded-lg border border-down-bad border-opacity-30 bg-down-bad bg-opacity-10">
+          <Typography variant="h6" color="down-bad">
+            Expired
+          </Typography>
+        </Box>
+      ) : null}
     </Box>
   );
 };
