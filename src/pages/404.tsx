f80190f7cdbb3a3fac446da866610fc0a375df61
hellodespereaux <hellodespereaux@gmail.com>
2022-10-07 10:29:39 +0200

    feat: added redirect for 404 page not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export default function Custom404() {
+  useEffect(() => {
+    window.location.replace('https://app.dopex.io');
+  }, []);
+
+  return <h1>404 - Page Not Found</h1>;
+}
