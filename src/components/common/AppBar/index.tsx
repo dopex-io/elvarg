258b2673a6727c3d66d27160648e3a8d455b75a6
hibou-de-nuit <hp@dopex.io>
2022-10-11 13:08:25 +0400

    fix: user asset balances

--- src/components/common/AppBar/index.tsx
+++ src/components/common/AppBar/index.tsx
@@ -147,6 +147,7 @@ export default function AppBar(props: AppBarProps) {
   const {
     accountAddress,
     connect,
+    provider,
     wrongNetwork,
     chainId,
     ensName,
@@ -207,7 +208,7 @@ export default function AppBar(props: AppBarProps) {
 
   useEffect(() => {
     updateAssetBalances();
-  }, [updateAssetBalances]);
+  }, [updateAssetBalances, provider]);
 
   const menuItems = useMemo(() => {
     return [
