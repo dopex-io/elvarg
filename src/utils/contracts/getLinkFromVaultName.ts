b053881c3690b28c76df4944bad5766baa7d74e4
hellodespereaux <hellodespereaux@gmail.com>
2022-11-07 21:05:38 +0100

    fix: adjust portfolio links

--- src/utils/contracts/getLinkFromVaultName.ts
+++ src/utils/contracts/getLinkFromVaultName.ts
@@ -1,14 +1,5 @@
 export default function getLinkFromVaultName(vaultName: string): string {
-  if (
-    vaultName.includes('V3') &&
-    !vaultName.includes('V3 2') &&
-    !vaultName.includes('V3 3')
-  ) {
-    return `https://ssov-epoch-6.dopex.io/ssov/${vaultName.replaceAll(
-      ' ',
-      '-'
-    )}`;
-  } else if (vaultName.includes('V3'))
+  if (vaultName.includes('V3 2') || vaultName.includes('V3 3'))
     return `/ssov-v3/${vaultName.replaceAll(' ', '-')}`;
 
   return `#`;
