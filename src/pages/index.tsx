0609fe6ceb0ec7b0f457972d459ec8d42e718b50
WitherBlock <witherblock@protonmail.com>
2023-01-19 00:24:04 +0400

    feat: made it more lively

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,15 +17,17 @@ interface CardProps {
   href: string;
   Icon: any;
 }
-
+//hover:bg-gradient-to-l from-wave-blue
 const Card = ({ name, description, href, Icon }: CardProps) => {
   return (
     <Link href={href}>
-      <Box className="bg-cod-gray shadow-2xl p-4 rounded-md flex space-x-4 items-center hover:-translate-y-1 transition ease-in hover:bg-gradient-to-l from-stone-900 to-transparent cursor-pointer">
+      <Box className="bg-umbra shadow-2xl p-4 rounded-2xl flex space-x-4 items-center hover:-translate-y-1 transition ease-in hover:backdrop-blur-md hover:bg-transparent cursor-pointer">
         <Icon className="w-8 h-8" />
         <Box>
-          <Typography variant="h5">{name}</Typography>
-          <Typography variant="h6" color="stieglitz">
+          <Typography variant="h5" className="font-bold">
+            {name}
+          </Typography>
+          <Typography variant="h6" color="stieglitz" className="font-bold">
             {description}
           </Typography>
         </Box>
@@ -37,38 +39,52 @@ const Card = ({ name, description, href, Icon }: CardProps) => {
 const Home = () => {
   return (
     <Box
-      className="bg-black min-h-screen"
-      sx={{
-        background: 'linear-gradient(219deg, #001a37, #101010, #003451)',
-        backgroundSize: '600% 600%',
-      }}
+      // className="bg-[url('/assets/vaults-background.png')] bg-left-top bg-contain bg-no-repeat min-h-screen"
+      className="bg-[url('/assets/vaults-background.png')] bg-left-top bg-contain bg-no-repeat min-h-screen"
+      // sx={{
+      //   background: 'linear-gradient(219deg, #001a37, #101010, #003451)',
+      //   backgroundSize: '600% 600%',
+      // }}
     >
       <Head>
         <title>Home | Dopex</title>
       </Head>
       <AppBar />
-      <Box className="pb-28 pt-40 lg:max-w-4xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0 to-">
-        <Box className="flex justify-between mb-24">
+      <Box className="pb-28 pt-40 lg:max-w-5xl md:max-w-3xl sm:max-w-xl max-w-md mx-auto px-4 lg:px-0 to-">
+        <Box className="flex space-x-8">
+          <img
+            src="/images/brand/logo.svg"
+            alt="logo"
+            className="md:w-28 md:h-28 w-20 h-20"
+          />
+          <h1 className="md:text-9xl text-7xl font-mono font-bold mb-24">
+            DOPEX
+          </h1>
+        </Box>
+        <Box className="flex md:flex-row md:space-y-0 space-y-8 flex-col justify-between mb-24">
           <Box className="flex flex-col max-w-fit">
-            <h1 className="text-9xl font-mono font-bold">400M+</h1>
-            <span className="text-2xl text-stieglitz self-end font-bold font-mono">
-              Open Interest{' '}
-              <span className="text-2xl text-stieglitz">(All Time)</span>
+            <h1 className="md:text-8xl text-6xl  font-mono font-bold text-wave-blue">
+              $400M+
+            </h1>
+            <span className="text-2xl text-white self-end font-bold font-mono">
+              All Time Open Interest
             </span>
           </Box>
           <Box className="flex flex-col max-w-fit">
-            <h1 className="text-9xl font-mono font-bold">21.6M</h1>
-            <span className="text-2xl text-stieglitz self-end font-bold font-mono">
+            <h1 className="md:text-8xl text-6xl  font-mono font-bold text-wave-blue">
+              $32M+
+            </h1>
+            <span className="text-2xl text-white self-end font-bold font-mono">
               Total Value Locked
             </span>
           </Box>
         </Box>
 
         <Box className="">
-          <Typography variant="h2" className="mb-8">
+          <Typography variant="h2" className="mb-8 font-bold">
             Our Products
           </Typography>
-          <Box className="grid grid-cols-2 gap-8">
+          <Box className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <Card
               name="Single Staking Option Vaults"
               description="Sell covered options to earn yields"
@@ -78,7 +94,7 @@ const Home = () => {
             <Card
               name="Farms"
               description="Earn rewards for liquidity staking"
-              href="/ssov"
+              href="/farms"
               Icon={AgricultureIcon}
             />
             <Card
@@ -90,13 +106,13 @@ const Home = () => {
             <Card
               name="Governance"
               description="Lock DPX to earn protocol fees and rewards"
-              href="/ssov"
+              href="/governance/vedpx"
               Icon={GavelIcon}
             />
             <Card
               name="Option Liquidity Pools"
               description="Purchase SSOV options at a discounted IV"
-              href="/ssov"
+              href="/olp/DPX-MONTHLY"
               Icon={AccountTreeIcon}
             />
           </Box>
