c504c698970cc4c847b5a4e62f6006fab22805db
WitherBlock <witherblock@protonmail.com>
2023-01-20 23:17:03 +0400

    chore: revert "add geolocation blocking"
    This reverts commit dd577af77ba638e26fd80f2a0cca45c79a20622b.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react';
 import StylesProvider from '@mui/styles/StylesProvider';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
-import axios from 'axios';
 import type { AppProps } from 'next/app';
-
-import { BLOCKED_COUNTRIES_ALPHA_2_CODES } from 'constants/index';
 
 import ChangeNetworkDialog from 'components/common/ChangeNetworkDialog';
 import theme from '../style/muiTheme';
@@ -14,37 +10,6 @@ import 'tailwindcss/tailwind.css';
 import '../style/index.css';
 
 function App({ Component, pageProps }: AppProps) {
-  const [state, setState] = useState({ isOfac: false });
-
-  useEffect(() => {
-    async function checkLocation() {
-      try {
-        const { data } = await axios.get(
-          'https://www.cloudflare.com/cdn-cgi/trace'
-        );
-        const countryAlpha2Code = data.split('loc=')[1].substring(0, 2);
-        if (BLOCKED_COUNTRIES_ALPHA_2_CODES.includes(countryAlpha2Code)) {
-          setState({ isOfac: true });
-        } else {
-          setState({ isOfac: false });
-        }
-      } catch {
-        setState({ isOfac: false });
-      }
-    }
-
-    checkLocation();
-  }, []);
-
-  if (state.isOfac) {
-    return (
-      <div>
-        You are connected from a restricted country and are not permitted to use
-        the platform.
-      </div>
-    );
-  }
-
   return (
     <StylesProvider injectFirst>
       <StyledEngineProvider injectFirst>
