1f45d03f8958ce64d8fde1c61cc376167c250ad0
WitherBlock <witherblock@protonmail.com>
2023-01-27 04:18:57 +0400

    chore: remove legacy folder

--- legacy/scripts/contracts.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-
-shell.cd('../farm-contracts');
-
-shell.exec('node ./scripts/runFrontendMigration.js');
-
-shell.cd('../token-sale');
-
-shell.exec('node ./scripts/runFrontendMigration.js');
