c6c2409715b88305433b6a48af6830cc655a7589
WitherBlock <witherblock@protonmail.com>
2023-02-09 03:42:45 +0400

    refactor: remove all OTC code

--- apps/dapp/src/constants/env.ts
+++ apps/dapp/src/constants/env.ts
@@ -2,11 +2,6 @@ export const INFURA_PROJECT_ID = process.env['NEXT_PUBLIC_INFURA_PROJECT_ID'];
 
 export const ANKR_KEY = process.env['NEXT_PUBLIC_ANKR_KEY'];
 
-export const FIREBASE_KEY = process.env['NEXT_PUBLIC_FIREBASE_API_KEY'];
-
-export const FIREBASE_PROJECT_ID =
-  process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'];
-
 export const DEFAULT_CHAIN_ID =
   Number(process.env['NEXT_PUBLIC_DEFAULT_CHAIN_ID']) ?? 42161;
 
