3f3eccfa00bb1c2b21d5022620f2477229938a9f
garyunwin42 <garyunwin42@gmail.com>
2023-02-13 20:39:26 -0500

    feat: add link for featured

--- apps/dapp/src/components/olp/OlpHome/FeaturedOlp.tsx
+++ apps/dapp/src/components/olp/OlpHome/FeaturedOlp.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 import Box from '@mui/material/Box';
 import { Divider } from '@mui/material';
@@ -52,73 +53,75 @@ export const FeaturedOlp = ({ olp }: { olp: IOlpApi | undefined }) => {
 
   return (
     <Box className="cursor-pointer border border-cod-gray hover:border-wave-blue rounded-lg flex-1 p-1 bg-cod-gray">
-      <Box className="flex p-2">
-        <Box className="w-7 h-7 border border-gray-500 rounded-full mr-2">
-          <img
-            src={`/images/tokens/${olp.underlyingSymbol.toLowerCase()}.svg`}
-            alt={olp.underlyingSymbol}
-          />
+      <Link href={`/olp/${olp.symbol}`} passHref target="_blank">
+        <Box className="flex p-2">
+          <Box className="w-7 h-7 border border-gray-500 rounded-full mr-2">
+            <img
+              src={`/images/tokens/${olp.underlyingSymbol.toLowerCase()}.svg`}
+              alt={olp.underlyingSymbol}
+            />
+          </Box>
+          <Typography variant="h6" color="white" className="capitalize my-auto">
+            {`${splitSymbol[0]} ${splitSymbol[1]?.toLowerCase()}`}
+          </Typography>
+          <Box className="ml-auto">
+            <img
+              src={CHAIN_ID_TO_NETWORK_DATA[olp.chainId]?.icon}
+              alt={CHAIN_ID_TO_NETWORK_DATA[olp.chainId]?.name}
+              className="w-6 h-auto"
+            />
+          </Box>
         </Box>
-        <Typography variant="h6" color="white" className="capitalize my-auto">
-          {`${splitSymbol[0]} ${splitSymbol[1]?.toLowerCase()}`}
-        </Typography>
-        <Box className="ml-auto">
-          <img
-            src={CHAIN_ID_TO_NETWORK_DATA[olp.chainId]?.icon}
-            alt={CHAIN_ID_TO_NETWORK_DATA[olp.chainId]?.name}
-            className="w-6 h-auto"
-          />
-        </Box>
-      </Box>
-      <ResponsiveContainer width="99%" height="35%" className="my-3">
-        {/* TODO: use utilizationData */}
-        <LineChart data={fakeData.map((d) => ({ utilization: d }))}>
+        <ResponsiveContainer width="99%" height="35%" className="my-3">
           {/* TODO: use utilizationData */}
-          <Tooltip
-            cursor={false}
-            wrapperStyle={{ outline: 'none' }}
-            content={<CustomizedTooltip payload={fakeData} />}
-          />
-          <Line
-            className="blur-sm"
-            type="monotone"
-            dataKey="utilization"
-            stroke="#22E1FF"
-            dot={false}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-      <Divider className="fill-current bg-carbon" />
-      <Box className="flex justify-between rounded-b-lg p-1 mt-1">
-        <Box className=" flex flex-col flex-1 items-center">
-          <Typography variant="h6" color="white">
-            $
-            <NumberDisplay n={BigNumber.from(olp.tvl)} decimals={0} />
-          </Typography>
-          <Typography variant="h6" color="stieglitz">
-            TVL
-          </Typography>
+          <LineChart data={fakeData.map((d) => ({ utilization: d }))}>
+            {/* TODO: use utilizationData */}
+            <Tooltip
+              cursor={false}
+              wrapperStyle={{ outline: 'none' }}
+              content={<CustomizedTooltip payload={fakeData} />}
+            />
+            <Line
+              className="blur-sm"
+              type="monotone"
+              dataKey="utilization"
+              stroke="#22E1FF"
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+        <Divider className="fill-current bg-carbon" />
+        <Box className="flex justify-between rounded-b-lg p-1 mt-1">
+          <Box className=" flex flex-col flex-1 items-center">
+            <Typography variant="h6" color="white">
+              $
+              <NumberDisplay n={BigNumber.from(olp.tvl)} decimals={0} />
+            </Typography>
+            <Typography variant="h6" color="stieglitz">
+              TVL
+            </Typography>
+          </Box>
+          <Box className=" flex flex-col flex-1 items-center">
+            <Typography variant="h6" color="wave-blue">
+              {olp.tvl === 0
+                ? '0'
+                : Math.round((olp.utilization / olp.tvl) * 100)}
+              %
+            </Typography>
+            <Typography variant="h6" color="stieglitz">
+              Utilization
+            </Typography>
+          </Box>
+          <Box className=" flex flex-col flex-1 items-center">
+            <Typography variant="h6" className="w-max">
+              {getReadableTime(olp.expiry)}
+            </Typography>
+            <Typography variant="h6" color="stieglitz">
+              Expiry
+            </Typography>
+          </Box>
         </Box>
-        <Box className=" flex flex-col flex-1 items-center">
-          <Typography variant="h6" color="wave-blue">
-            {olp.tvl === 0
-              ? '0'
-              : Math.round((olp.utilization / olp.tvl) * 100)}
-            %
-          </Typography>
-          <Typography variant="h6" color="stieglitz">
-            Utilization
-          </Typography>
-        </Box>
-        <Box className=" flex flex-col flex-1 items-center">
-          <Typography variant="h6" className="w-max">
-            {getReadableTime(olp.expiry)}
-          </Typography>
-          <Typography variant="h6" color="stieglitz">
-            Expiry
-          </Typography>
-        </Box>
-      </Box>
+      </Link>
     </Box>
   );
 };
