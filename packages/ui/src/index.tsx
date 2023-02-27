bfbb2b6231464a62a6da06700a1101757aa5a104
Komorebi <109383132+garyunwin42@users.noreply.github.com>
2023-02-26 20:03:38 -0500

    feat: add tab UI component (#427)

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import Switch from "./Switch";
-
+import TabHeader from "./TabHeader";
+import Tabs from "./Tabs";
 import tailwindThemeConfig from "./tailwindThemeConfig.json";
 
-export { Switch, Button, tailwindThemeConfig };
+export { Switch, Button, Tabs, TabHeader, tailwindThemeConfig };
---
+++ packages/ui/stories/Tabs.stories.tsx
@@ -0,0 +1,67 @@
+import AlarmIcon from "../../../apps/dapp/src/svgs/icons/AlarmIcon";
+import TabHeader from "../src/TabHeader";
+import Tabs from "../src/Tabs";
+import { ComponentMeta } from "@storybook/react";
+import React from "react";
+
+export default {
+  title: "Tabs",
+  component: Tabs,
+} as ComponentMeta<typeof Tabs>;
+
+const Template = (args) => {
+  return (
+    <div className="max-w-md px-2 py-16 sm:px-0">
+      {args.array.map((size) => {
+        return (
+          <Tabs size={size}>
+            <TabHeader>item one</TabHeader>
+            <TabHeader>item two</TabHeader>
+            <TabHeader>item three</TabHeader>
+          </Tabs>
+        );
+      })}
+    </div>
+  );
+};
+
+export const Sizes = Template.bind({});
+Sizes.args = {
+  array: ["large", "medium", "small"],
+};
+
+export const Disabled = () => {
+  return (
+    <div className="max-w-md px-2 py-16 sm:px-0">
+      <Tabs>
+        <TabHeader>item one</TabHeader>
+        <TabHeader disabled>item two</TabHeader>
+        <TabHeader>item three</TabHeader>
+      </Tabs>
+    </div>
+  );
+};
+
+export const Icons = () => {
+  return (
+    <div className="max-w-md px-2 py-16 sm:px-0">
+      <Tabs>
+        <TabHeader>
+          <div className="flex justify-around">
+            <AlarmIcon />
+          </div>
+        </TabHeader>
+        <TabHeader>
+          <div className="flex justify-around">
+            <AlarmIcon />
+          </div>
+        </TabHeader>
+        <TabHeader>
+          <div className="flex justify-around">
+            <AlarmIcon />
+          </div>
+        </TabHeader>
+      </Tabs>
+    </div>
+  );
+};
