f3c2d090be90e3a0d789024a6bcb661a7c2e7b53
hellodespereaux <hellodespereaux@gmail.com>
2023-03-08 18:13:55 +0100

    feat: open close buttons

--- apps/dapp/src/components/scalps/Positions/PositionsTable.tsx
+++ apps/dapp/src/components/scalps/Positions/PositionsTable.tsx
@@ -22,7 +22,7 @@ import { TableHeader } from 'components/straddles/Deposits/DepositsTable';
 import getUserReadableAmount from 'utils/contracts/getUserReadableAmount';
 import formatAmount from 'utils/general/formatAmount';
 
-const PositionsTable = () => {
+const PositionsTable = ({ tab }: { tab: string }) => {
   const sendTx = useSendTx();
 
   const {
@@ -68,109 +68,134 @@ const PositionsTable = () => {
             </TableRow>
           </TableHead>
           <TableBody className="rounded-lg">
-            {optionScalpUserData?.scalpPositions?.map((position, i) => (
-              <TableRow key={i}>
-                <TableCell className="pt-2 border-0">
-                  <Box>
-                    <Box
-                      className={`rounded-md flex items-center px-2 py-2 w-fit`}
-                    >
-                      <Typography variant="h6" className={'pr-7 pt-[2px]'}>
-                        <span
-                          className={
-                            position.isShort
-                              ? 'text-[#FF617D]'
-                              : 'text-[#6DFFB9]'
-                          }
-                        >
-                          {position.isShort ? '-' : '+'}
-                          {formatAmount(
-                            getUserReadableAmount(position.positions, 8),
-                            8
-                          )}
-                          {' ETH'}
-                        </span>
-                      </Typography>
+            {optionScalpUserData?.scalpPositions?.map((position, i) =>
+              (tab === 'Open' && position.isOpen) ||
+              (tab === 'Closed' && !position.isOpen) ? (
+                <TableRow key={i}>
+                  <TableCell className="pt-2 border-0">
+                    <Box>
+                      <Box
+                        className={`rounded-md flex items-center px-2 py-2 w-fit`}
+                      >
+                        <Typography variant="h6" className={'pr-7 pt-[2px]'}>
+                          <span
+                            className={
+                              position.isShort
+                                ? 'text-[#FF617D]'
+                                : 'text-[#6DFFB9]'
+                            }
+                          >
+                            {position.isShort ? '-' : '+'}
+                            {formatAmount(
+                              getUserReadableAmount(position.positions, 8),
+                              8
+                            )}
+                            {' ETH'}
+                          </span>
+                        </Typography>
+                      </Box>
                     </Box>
-                  </Box>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" color="white" className="text-left">
-                    ${getUserReadableAmount(position.entry, 8).toFixed(2)}
-                  </Typography>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" color="white" className="text-left">
-                    $
-                    {getUserReadableAmount(
-                      position.liquidationPrice,
-                      8
-                    ).toFixed(2)}
-                  </Typography>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" className="text-left">
-                    <span
-                      className={
-                        position.pnl.lt(0) ? 'text-[#FF617D]' : 'text-[#6DFFB9]'
-                      }
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography
+                      variant="h6"
+                      color="white"
+                      className="text-left"
                     >
-                      ${getUserReadableAmount(position.pnl, 6).toFixed(2)}
-                    </span>
-                  </Typography>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" color="white" className="text-left">
-                    ${getUserReadableAmount(position.margin, 6).toFixed(2)}
-                  </Typography>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" color="white" className="text-left">
-                    ${getUserReadableAmount(position.premium, 6).toFixed(2)}
-                  </Typography>
-                </TableCell>
-                <TableCell className="pt-1 border-0">
-                  <Typography variant="h6" color="white" className="text-left">
-                    {position.isOpen ? (
-                      <Countdown
-                        date={
-                          new Date(
-                            Number(
-                              BigNumber.from('1000').mul(
-                                position.openedAt.add(position.timeframe)
+                      ${getUserReadableAmount(position.entry, 8).toFixed(2)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography
+                      variant="h6"
+                      color="white"
+                      className="text-left"
+                    >
+                      $
+                      {getUserReadableAmount(
+                        position.liquidationPrice,
+                        8
+                      ).toFixed(2)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography variant="h6" className="text-left">
+                      <span
+                        className={
+                          position.pnl.lt(0)
+                            ? 'text-[#FF617D]'
+                            : 'text-[#6DFFB9]'
+                        }
+                      >
+                        ${getUserReadableAmount(position.pnl, 6).toFixed(2)}
+                      </span>
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography
+                      variant="h6"
+                      color="white"
+                      className="text-left"
+                    >
+                      ${getUserReadableAmount(position.margin, 6).toFixed(2)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography
+                      variant="h6"
+                      color="white"
+                      className="text-left"
+                    >
+                      ${getUserReadableAmount(position.premium, 6).toFixed(2)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="pt-1 border-0">
+                    <Typography
+                      variant="h6"
+                      color="white"
+                      className="text-left"
+                    >
+                      {position.isOpen ? (
+                        <Countdown
+                          date={
+                            new Date(
+                              Number(
+                                BigNumber.from('1000').mul(
+                                  position.openedAt.add(position.timeframe)
+                                )
                               )
                             )
-                          )
-                        }
-                        renderer={({ minutes, seconds }) => {
-                          return (
-                            <Typography
-                              variant="h5"
-                              className="text-stieglitz mr-1"
-                            >
-                              {minutes}m {seconds}s
-                            </Typography>
-                          );
-                        }}
-                      />
-                    ) : (
-                      <span className={'text-[#FF617D]'}>Closed</span>
-                    )}
-                  </Typography>
-                </TableCell>
-                {position.isOpen ? (
-                  <TableCell className="flex justify-end border-0">
-                    <CustomButton
-                      className="cursor-pointer text-white"
-                      color={'primary'}
-                      onClick={() => handleClose(position.id)}
-                    >
-                      Close
-                    </CustomButton>
+                          }
+                          renderer={({ minutes, seconds }) => {
+                            return (
+                              <Typography
+                                variant="h5"
+                                className="text-stieglitz mr-1"
+                              >
+                                {minutes}m {seconds}s
+                              </Typography>
+                            );
+                          }}
+                        />
+                      ) : (
+                        <span className={'text-[#FF617D]'}>Closed</span>
+                      )}
+                    </Typography>
                   </TableCell>
-                ) : null}
-              </TableRow>
-            ))}
+                  {position.isOpen ? (
+                    <TableCell className="flex justify-end border-0">
+                      <CustomButton
+                        className="cursor-pointer text-white"
+                        color={'primary'}
+                        onClick={() => handleClose(position.id)}
+                      >
+                        Close
+                      </CustomButton>
+                    </TableCell>
+                  ) : null}
+                </TableRow>
+              ) : null
+            )}
           </TableBody>
         </Table>
       </TableContainer>
--- apps/dapp/src/components/scalps/Positions/index.tsx
+++ apps/dapp/src/components/scalps/Positions/index.tsx
@@ -1,28 +1,50 @@
+import { useState } from 'react';
+
 import Box from '@mui/material/Box';
-import {  useBoundStore } from 'store';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
 
 import Typography from 'components/UI/Typography';
 
 import displayAddress from 'utils/general/displayAddress';
 
+import { useBoundStore } from 'store';
+
 import PositionsTable from './PositionsTable';
 
 const Positions = () => {
   const { accountAddress, ensName } = useBoundStore();
+  const [activeTab, setActiveTab] = useState<string>('Open');
 
   return (
     <Box className="text-gray-400 w-full rounded-lg">
       <Box className="border rounded-t-xl border-cod-gray py-2 bg-umbra">
         <Box className="flex ml-3">
-          <Box className="rounded-md bg-neutral-700 flex mb-2 mt-1">
+          <Box className="rounded-md bg-neutral-700 flex mb-2 mt-3 h-auto">
             <Typography variant="h6" className="ml-auto p-1">
               {displayAddress(accountAddress, ensName)}
             </Typography>
           </Box>
+          <ButtonGroup className="flex w-32 justify-between bg-cod-gray border border-umbra rounded-lg mb-3 ml-auto mr-4 mt-1">
+            {['Open', 'Closed'].map((label, index) => (
+              <Button
+                key={index}
+                className={`border-0 hover:border-0 w-full m-1 p-1 transition ease-in-out duration-500 ${
+                  activeTab === label
+                    ? 'text-white bg-carbon hover:bg-carbon'
+                    : 'text-stieglitz bg-transparent hover:bg-transparent'
+                } hover:text-white`}
+                disableRipple
+                onClick={() => setActiveTab(label)}
+              >
+                <Typography variant="h6">{label}</Typography>
+              </Button>
+            ))}
+          </ButtonGroup>
         </Box>
       </Box>
       <Box className="border rounded-b-xl border-cod-gray border-t-neutral-800 bg-umbra">
-        <PositionsTable />
+        <PositionsTable tab={activeTab} />
       </Box>
     </Box>
   );
