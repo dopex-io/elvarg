7b133f5fadb513240f5df6af7ba2d6244687e966
hibou-de-nuit <hp@dopex.io>
2023-03-20 17:11:57 +0400

    fix: fix import caused by svgr/cli

--- packages/ui/src/icons/Usdc.tsx
+++ packages/ui/src/icons/Usdc.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 
 const Usdc = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
