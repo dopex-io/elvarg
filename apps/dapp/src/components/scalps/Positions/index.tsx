f691fe2d019d164b2db8f853071566cccff79f7a
Aercwarden <arc@dopex.io>
2023-03-29 21:28:22 +0400

    feat: rework position table and share card logic

--- apps/dapp/src/components/scalps/Positions/index.tsx
+++ apps/dapp/src/components/scalps/Positions/index.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 
-import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
-
-import Typography from 'components/UI/Typography';
 
 import displayAddress from 'utils/general/displayAddress';
 
@@ -17,14 +14,14 @@ const Positions = () => {
   const [activeTab, setActiveTab] = useState<string>('Open');
 
   return (
-    <Box className="text-gray-400 min-w-[60vw] rounded-lg">
-      <Box className="border rounded-t-xl border-cod-gray py-2 bg-umbra">
-        <Box className="flex ml-3">
-          <Box className="rounded-md bg-neutral-700 flex mb-2 mt-3 h-auto">
-            <Typography variant="h6" className="ml-auto p-1 text-[0.8rem]">
+    <div className="text-gray-400 min-w-[60vw] rounded-lg">
+      <div className="border rounded-t-xl border-cod-gray py-2 bg-umbra">
+        <div className="flex ml-3">
+          <div className="rounded-md bg-neutral-700 flex mb-2 mt-3 h-auto">
+            <span className="ml-auto p-1 text-white text-xs">
               {displayAddress(accountAddress, ensName)}
-            </Typography>
-          </Box>
+            </span>
+          </div>
           <ButtonGroup className="flex w-32 justify-between bg-cod-gray border border-umbra rounded-lg mb-3 ml-auto mr-4 mt-1">
             {['Open', 'Closed'].map((label, index) => (
               <Button
@@ -37,16 +34,20 @@ const Positions = () => {
                 disableRipple
                 onClick={() => setActiveTab(label)}
               >
-                <Typography variant="h6">{label}</Typography>
+                <span
+                  className={`${activeTab === label && 'text-white'} text-xs`}
+                >
+                  {label}
+                </span>
               </Button>
             ))}
           </ButtonGroup>
-        </Box>
-      </Box>
-      <Box className="border rounded-b-xl border-cod-gray border-t-neutral-800 bg-umbra">
+        </div>
+      </div>
+      <div className="border rounded-b-xl border-cod-gray border-t-neutral-800 bg-umbra">
         <PositionsTable tab={activeTab} />
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 };
 
