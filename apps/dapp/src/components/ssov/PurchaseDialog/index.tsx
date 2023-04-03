a5e98f7f54b6e0d65860c4ef45be71c00f4a7951
HolyPepperone <85767768+hibou-de-nuit@users.noreply.github.com>
2023-04-03 23:32:43 +0400

    fix(ssov): deposit panel (#482)

--- apps/dapp/src/components/ssov/PurchaseDialog/index.tsx
+++ apps/dapp/src/components/ssov/PurchaseDialog/index.tsx
@@ -350,7 +350,7 @@ const PurchaseDialog = ({
       !ssovContractWithSigner ||
       !accountAddress ||
       !ssovSigner ||
-      !ssovSigner.ssovRouterWithSigner
+      !(chainId === 137 || ssovSigner.ssovRouterWithSigner)
     )
       return;
 
@@ -387,7 +387,7 @@ const PurchaseDialog = ({
     const method = routerMode ? 'swapAndPurchase' : ('purchase' as any);
 
     try {
-      await sendTx(contractWithSigner, method, params);
+      await sendTx(contractWithSigner!, method, params);
       setRawOptionsAmount('0');
       updateAssetBalances();
       updateSsovV3UserData();
@@ -549,7 +549,6 @@ const PurchaseDialog = ({
         quoteDataLoading
     );
 
-    console.log(optionsAmount <= 0);
 
     let onClick = () => {};
 
