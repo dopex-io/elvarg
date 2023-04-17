1fe64bd8e6a6e7c135ad333d4c2150968118c2bc
garyunwin42 <garyunwin42@gmail.com>
2023-04-16 20:28:20 -0400

    feat: test keeper

--- apps/dapp/src/pages/api/cron/index.tsx
+++ apps/dapp/src/pages/api/cron/index.tsx
@@ -24,8 +24,9 @@ export default async function handler(
   response: NextApiResponse
 ) {
   const keeper_pk = process.env['KEEPER_PK'];
+  const isKeeperValid = keeper_pk && validPk(keeper_pk);
 
-  if (!keeper_pk || !validPk(keeper_pk)) {
+  if (!isKeeperValid) {
     return response.status(500).json({ error: 'Invalid pk for keeper' });
   }
 
@@ -34,41 +35,47 @@ export default async function handler(
       new ethers.providers.StaticJsonRpcProvider(CHAINS[arbitrum.id]?.rpc)
     );
 
-    const wallet = new ethers.Wallet(keeper_pk!);
-    const signer = wallet.connect(provider);
+    // const wallet = new ethers.Wallet(keeper_pk!);
+    // const signer = wallet.connect(provider);
 
     const zdteContract = await Zdte__factory.connect(ZDTE, provider);
+    const price = await zdteContract.getMarkPrice();
 
-    async function callContractWithRetry() {
-      const maxRetries = 3; // maximum number of retries
-      let retryCount = 0;
+    // async function callContractWithRetry() {
+    //   const maxRetries = 3; // maximum number of retries
+    //   let retryCount = 0;
 
-      while (retryCount < maxRetries) {
-        try {
-          const tx = await zdteContract.connect(signer).keeperRun();
-          const receipt = await tx.wait();
-          const res = receipt.events ? receipt.events[0] : 'failed';
-          console.log('res: ', res);
-          return;
-        } catch (error) {
-          console.error(
-            `Failed to run keeper. Retrying... (${
-              retryCount + 1
-            }/${maxRetries})`
-          );
-          retryCount++;
-        }
-      }
+    //   while (retryCount < maxRetries) {
+    //     try {
 
-      // Throw an error if the call failed after all retries
-      throw new Error('Max retries reached. Call failed.');
-    }
+    //       // const tx = await zdteContract.connect(signer).keeperRun();
+    //       // const receipt = await tx.wait();
+    //       // const res = receipt.events ? receipt.events[0] : 'failed';
+    //       // console.log('res: ', res);
+    //       return;
+    //     } catch (error) {
+    //       console.error(
+    //         `Failed to run keeper. Retrying... (${
+    //           retryCount + 1
+    //         }/${maxRetries})`
+    //       );
+    //       retryCount++;
+    //     }
+    //   }
 
-    callContractWithRetry().catch((e) => {
-      return response.status(500).json({ error: e.message });
+    //   // Throw an error if the call failed after all retries
+    //   throw new Error('Max retries reached. Call failed.');
+    // }
+
+    // callContractWithRetry().catch((e) => {
+    //   return response.status(500).json({ error: e.message });
+    // });
+
+    return response.status(200).json({
+      success: true,
+      isKeeperValid: isKeeperValid,
+      price: price.toNumber(),
     });
-
-    return response.status(200).json({ success: 'true' });
   } catch (error) {
     return response.status(500).json({ error: error });
   }
