235f09a4666e9d8eb87a5b6e81aae6a79b465dda
garyunwin42 <garyunwin42@gmail.com>
2023-04-20 00:31:53 -0400

    feat: dont stretch table

--- apps/dapp/src/components/zdte/Positions/index.tsx
+++ apps/dapp/src/components/zdte/Positions/index.tsx
@@ -19,7 +19,7 @@ const Positions = () => {
   }
 
   return (
-    <div className="text-gray-400 min-w-[60vw] rounded-lg">
+    <div className="text-gray-400 w-full rounded-lg">
       <div className="border rounded-t-xl border-cod-gray py-2 bg-umbra">
         <div className="flex ml-3">
           <div className="rounded-md bg-neutral-700 flex mb-2 mt-3 h-auto">
