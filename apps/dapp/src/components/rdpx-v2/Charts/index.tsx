7d3452368588b192235cc3e4f22862909a90e008
hibou-de-nuit <hp@dopex.io>
2023-04-22 13:12:30 +0400

    fix: build

--- apps/dapp/src/components/rdpx-v2/Charts/index.tsx
+++ apps/dapp/src/components/rdpx-v2/Charts/index.tsx
@@ -4,18 +4,15 @@ const ClientRenderedPriceChart = dynamic(() => import('./PriceChart'), {
   ssr: false,
 });
 
-const ClientRenderedDpxethSupplyChart = dynamic(
-  () => import('./DpxethSupply'),
-  {
-    ssr: false,
-  }
-);
+const ClientRenderedSupplyChart = dynamic(() => import('./SupplyChart'), {
+  ssr: false,
+});
 
 const Charts = () => {
   return (
     <div className="flex flex-col sm:flex-col md:flex-row space-y-3 sm:space-y-3 md:space-y-0 sm:space-x-0 md:space-x-3">
       <div className="flex flex-col bg-cod-gray rounded-lg divide-y divide-umbra w-full md:w-1/2 sm:w-full">
-        <ClientRenderedDpxethSupplyChart data={[]} width={255} height={167.5} />
+        <ClientRenderedSupplyChart data={[]} width={255} height={167.5} />
       </div>
       <div className="flex flex-col bg-cod-gray rounded-lg divide-y divide-umbra w-full md:w-1/2 sm:w-full">
         <ClientRenderedPriceChart data={[]} width={900} height={180} />
