743e32831ad0d0c3dce67d57ee3182c067c20e66
garyunwin42 <garyunwin42@gmail.com>
2023-04-25 22:43:25 -0400

    fix: remove comment

--- apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
+++ apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
@@ -1,8 +1,6 @@
 import DexScreenerChart from 'components/common/DexScreenerChart';
 
 const ZdteDexScreenerChart = () => {
-  // const { staticZdteData } = useBoundStore();
-  // TODO: read from contract?
   return (
     <DexScreenerChart
       poolAddress={'0xc31e54c7a869b9fcbecc14363cf510d1c41fa443'}
