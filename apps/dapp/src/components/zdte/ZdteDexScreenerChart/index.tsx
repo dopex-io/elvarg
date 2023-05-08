ac61176700836f670303b92ae3c4e91fdf661f54
hibou-de-nuit <hp@dopex.io>
2023-05-08 22:46:32 +0400

    perf(zdte): load times test commit

--- apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
+++ apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
@@ -2,9 +2,7 @@ import DexScreenerChart from 'components/common/DexScreenerChart';
 
 const ZdteDexScreenerChart = () => {
   return (
-    <DexScreenerChart
-      poolAddress={'0xc31e54c7a869b9fcbecc14363cf510d1c41fa443'}
-    />
+    <DexScreenerChart poolAddress="0xc31e54c7a869b9fcbecc14363cf510d1c41fa443" />
   );
 };
 
