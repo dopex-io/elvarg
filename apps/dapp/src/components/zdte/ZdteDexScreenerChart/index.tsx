d15c3781f7fe0e112d4e630021ed4ad2410988f0
garyunwin42 <garyunwin42@gmail.com>
2023-05-09 17:59:14 -0400

    fix: handle multiple addrs

--- apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
+++ apps/dapp/src/components/zdte/ZdteDexScreenerChart/index.tsx
@@ -4,15 +4,17 @@ interface Props {
   poolName: string;
 }
 
+const assetToContractAddress: Record<string, string> = {
+  eth: '0xc31e54c7a869b9fcbecc14363cf510d1c41fa443',
+  arb: '0xcDa53B1F66614552F834cEeF361A8D12a0B8DaD8',
+};
+
 const ZdteDexScreenerChart = (props: Props) => {
   const { poolName } = props;
+
   return (
     <DexScreenerChart
-      poolAddress={
-        poolName.toLowerCase() === 'eth'
-          ? '0xc31e54c7a869b9fcbecc14363cf510d1c41fa443'
-          : '0xcDa53B1F66614552F834cEeF361A8D12a0B8DaD8'
-      }
+      poolAddress={assetToContractAddress[poolName.toLowerCase()]!}
     />
   );
 };
