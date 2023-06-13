6b82bc59a5e141e8894b4cea4c5c909d6935125c
WitherBlock <79846912+witherblock@users.noreply.github.com>
2023-06-13 20:35:46 +0400

    feat: add skeleton ui (DOP-413) (#554)

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,11 +1,14 @@
-export { default as Button } from "./Button";
-export type { ButtonProps } from "./Button";
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
 
-export { default as Switch } from "./Switch";
-export { default as Tab } from "./Tab";
-export { default as TabHeader } from "./TabHeader";
-export { default as Menu } from "./Menu";
-export { default as MenuItems } from "./MenuItems";
-export { default as Input } from "./Input";
+export { default as Switch } from './Switch';
+export { default as Tab } from './Tab';
+export { default as TabHeader } from './TabHeader';
+export { default as Menu } from './Menu';
+export { default as MenuItems } from './MenuItems';
+export { default as Input } from './Input';
 
-export * from "./icons";
+export { default as Skeleton } from './Skeleton';
+export type { SkeletonProps } from './Skeleton';
+
+export * from './icons';
