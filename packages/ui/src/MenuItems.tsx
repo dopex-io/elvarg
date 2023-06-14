1da0e2c1dd5e7765d4063de7cb9b90ac930b72b1
WitherBlock <79846912+witherblock@users.noreply.github.com>
2023-06-14 10:55:26 +0400

    feat: add dialog to dopex ui (DOP-412) (#553)

--- packages/ui/src/MenuItems.tsx
+++ packages/ui/src/MenuItems.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { Menu } from "@headlessui/react";
+import React from 'react';
 
-export type dropdownVariants = "basic" | "icon" | "dense";
+import { Menu } from '@headlessui/react';
+
+export type dropdownVariants = 'basic' | 'icon' | 'dense';
 
 const COMMON_CLASSES = {
-  background: "bg-carbon",
-  textSize: "text-xs",
-  textColor: "text-white",
-  ddFill: "bg-umbra",
+  background: 'bg-carbon',
+  textSize: 'text-xs',
+  textColor: 'text-white',
+  ddFill: 'bg-umbra',
   dropDownPaper:
-    "absolute left-0 mt-2 w-56 origin-top-right rounded-lg bg-umbra shadow-lg focus:outline-none",
+    'absolute left-0 mt-2 w-56 origin-top-right rounded-lg bg-umbra shadow-lg focus:outline-none',
 };
 
 const VARIANT_CLASSES: Record<
@@ -18,17 +19,17 @@ const VARIANT_CLASSES: Record<
 > = {
   basic: {
     icons: null,
-    padding: "p-2",
+    padding: 'p-2',
     ...COMMON_CLASSES,
   },
   icon: {
     icons: null,
-    padding: "p-2",
+    padding: 'p-2',
     ...COMMON_CLASSES,
   },
   dense: {
-    icons: "hidden",
-    padding: "p-1",
+    icons: 'hidden',
+    padding: 'p-1',
     ...COMMON_CLASSES,
   },
 };
@@ -38,7 +39,7 @@ export type ItemType = Record<
   string | boolean | number | JSX.Element | undefined
 >;
 
-interface MenuItemsProps<T extends ItemType> {
+export interface MenuItemsProps<T extends ItemType> {
   data: T[];
   variant?: dropdownVariants;
   scrollable?: boolean;
@@ -52,7 +53,7 @@ const MenuItems: React.FC<MenuItemsProps<ItemType>> = <T extends ItemType>(
   const {
     data,
     handleSelection,
-    variant = "basic",
+    variant = 'basic',
     scrollable = false,
     topElement = null,
     ...rest
@@ -68,7 +69,7 @@ const MenuItems: React.FC<MenuItemsProps<ItemType>> = <T extends ItemType>(
       {topElement}
       <div
         className={`p-1 min-h-fit ${
-          scrollable ? "max-h-32 overflow-auto" : null
+          scrollable ? 'max-h-32 overflow-auto' : null
         }`}
       >
         {data.map((dataItem) => (
@@ -76,23 +77,23 @@ const MenuItems: React.FC<MenuItemsProps<ItemType>> = <T extends ItemType>(
             {({ active }: { active: boolean }) => (
               <button
                 className={`${
-                  selectedVariant["padding"]
+                  selectedVariant['padding']
                 } flex justify-between rounded-md w-full ${
-                  active ? "bg-carbon" : "bg-umbra"
+                  active ? 'bg-carbon' : 'bg-umbra'
                 } text-sm ${
-                  Boolean(Object(dataItem)["disabled"])
-                    ? "bg-opacity-50 text-stieglitz cursor-not-allowed"
-                    : "text-white cursor-pointer"
+                  Boolean(Object(dataItem)['disabled'])
+                    ? 'bg-opacity-50 text-stieglitz cursor-not-allowed'
+                    : 'text-white cursor-pointer'
                 }`}
                 onClick={handleSelection}
-                disabled={Boolean(Object(dataItem)["disabled"])}
+                disabled={Boolean(Object(dataItem)['disabled'])}
               >
                 <div className="flex space-x-2">
-                  {dataItem["icon"] && variant === "icon"
-                    ? (dataItem["icon"] as JSX.Element)
+                  {dataItem['icon'] && variant === 'icon'
+                    ? (dataItem['icon'] as JSX.Element)
                     : null}
                   <div className="flex justify-between">
-                    {Object(dataItem)["textContent"]}
+                    {Object(dataItem)['textContent']}
                   </div>
                 </div>
               </button>
@@ -104,6 +105,6 @@ const MenuItems: React.FC<MenuItemsProps<ItemType>> = <T extends ItemType>(
   );
 };
 
-MenuItems.displayName = "MenuItems";
+MenuItems.displayName = 'MenuItems';
 
 export default MenuItems;
