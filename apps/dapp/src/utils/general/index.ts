a7c5216ae2bf2f4c08e3dd303c2794a80beedc0f
Komorebi <109383132+garyunwin42@users.noreply.github.com>
2023-06-22 07:19:26 -0400

    feat: show straddles closed positions (#568)
    Co-authored-by: WitherBlock <witherblock@protonmail.com>

--- apps/dapp/src/utils/general/index.ts
+++ apps/dapp/src/utils/general/index.ts
@@ -5,6 +5,7 @@ export { default as getDEXFrom1inchName } from './getDEXFrom1inchName';
 export { default as getTokenDecimals } from './getTokenDecimals';
 export { default as formatAmount } from './formatAmount';
 export { default as getExplorerUrl } from './getExplorerUrl';
+export { default as getExplorerTxURL } from './getExplorerTxURL';
 export { default as getValueColorClass } from './getValueColorClass';
 export { default as getExtendedLogoFromChainId } from './getExtendedLogoFromChainId';
 export { default as smartTrim } from './smartTrim';
