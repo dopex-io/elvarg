4a9f86e6798e03f024c64a535c4eda26c7c88ce1
WitherBlock <witherblock@protonmail.com>
2023-07-04 20:47:09 +0400

    chore: remove TV chart

--- apps/dapp/src/pages/vaults/index.tsx
+++ apps/dapp/src/pages/vaults/index.tsx
@@ -1,16 +1,11 @@
-import { useCallback, useState } from 'react';
-import dynamic from 'next/dynamic';
-import Script from 'next/script';
+import { useCallback, useEffect, useState } from 'react';
 
-import { defaultChartProps } from 'config/tradingview';
-// import useDatafeed from 'hooks/useDatafeed';
-import useVaultState, { Vault } from 'hooks/vaults/state';
 import { NextSeo } from 'next-seo';
-import {
-  ChartingLibraryWidgetOptions,
-  ResolutionString,
-} from 'public/static/charting_library/charting_library';
+import { GmxCandleStick } from 'types';
 
+import useVaultState, { Vault } from 'hooks/vaults/state';
+
+import TVChart, { Period } from 'components/atlantics/InsuredPerps/TVChart';
 import PageLayout from 'components/common/PageLayout';
 import AsidePanel from 'components/vaults/AsidePanel';
 import Positions from 'components/vaults/Tables/Positions';
@@ -19,41 +14,11 @@ import TitleBar from 'components/vaults/TitleBar';
 
 import seo from 'constants/seo';
 
-const defaultWidgetProps: Partial<ChartingLibraryWidgetOptions> = {
-  theme: defaultChartProps.theme,
-  symbol: defaultChartProps.symbol,
-  locale: defaultChartProps.locale,
-  client_id: 'tradingview.com',
-  library_path: defaultChartProps.library_path,
-  charts_storage_api_version: defaultChartProps.charts_storage_api_version,
-  user_id: defaultChartProps.user_id,
-  overrides: defaultChartProps.overrides,
-  charts_storage_url: defaultChartProps.charts_storage_url,
-  custom_css_url: defaultChartProps.custom_css_url,
-  enabled_features: defaultChartProps.enabled_features,
-  disabled_features: defaultChartProps.disabled_features,
-  fullscreen: false,
-  autosize: true,
-  interval: defaultChartProps.interval as ResolutionString,
-};
-
-const TVChartContainer = dynamic(
-  () => import('components/common/TVContainer').then((mod) => mod.default),
-  { ssr: false }
-);
-
 const Vaults = () => {
   const update = useVaultState((vault) => vault.update);
   const vault = useVaultState((vault) => vault.vault);
-  // const datafeed = useDatafeed({ symbol: vault.base });
 
   const [selectedToken, setSelectedToken] = useState<string>('stETH');
-  const [widget, _] = useState<Partial<ChartingLibraryWidgetOptions>>({
-    ...defaultWidgetProps,
-    // symbol: vault.base,
-    symbol: 'AAPL', // todo: connect widget tp cg datafeed
-    // datafeed,
-  });
 
   const handleSelectToken = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,6 +30,59 @@ const Vaults = () => {
     },
     [update, vault]
   );
+
+  const [gmxChartData, setGmxChartData] = useState<GmxCandleStick[]>([]);
+  const [period, setPeriod] = useState<Period>('1D');
+
+  const updatePriceData = useCallback(async () => {
+    const res: Response = await new Promise(async (resolve, reject) => {
+      let done = false;
+      setTimeout(() => {
+        done = true;
+        reject(new Error(`Request timeout`));
+      }, 10000);
+
+      let lastEx;
+      for (let i = 0; i < 3; i++) {
+        if (done) return;
+        try {
+          const res = await fetch(
+            `https://stats.gmx.io/api/candles/${'ETH'}?preferableChainId=42161&period=${period}&from=${
+              Math.ceil(Number(new Date()) / 1000) - 86400 * 100
+            }&preferableSource=fast`
+          );
+          resolve(res);
+          return;
+        } catch (ex) {
+          lastEx = ex;
+        }
+      }
+      reject(lastEx);
+    });
+    if (!res.ok) throw new Error('request failed');
+    const json = await res.json();
+    let prices: GmxCandleStick[] = json?.prices.map(
+      (candleStickData: {
+        h: number;
+        l: number;
+        o: number;
+        c: number;
+        t: number;
+      }) => ({
+        high: candleStickData.h,
+        low: candleStickData.l,
+        open: candleStickData.o,
+        close: candleStickData.c,
+        time: candleStickData.t,
+      })
+    );
+
+    setGmxChartData(prices);
+  }, [period]);
+
+  useEffect(() => {
+    updatePriceData();
+  }, [updatePriceData]);
 
   return (
     <>
@@ -94,16 +112,20 @@ const Vaults = () => {
         />
         <div className="flex space-x-0 lg:space-x-6 flex-col sm:flex-col md:flex-col lg:flex-row space-y-3 md:space-y-0 justify-center">
           <div className="flex flex-col space-y-3 sm:w-full lg:w-3/4 h-full">
-            <div className="space-y-4 flex flex-col">
-              <div className="h-[420px] bg-carbon rounded-lg text-center flex flex-col justify-center text-stieglitz">
-                {
-                  <Script
-                    src="/static/datafeeds/udf/dist/bundle.js"
-                    strategy="lazyOnload"
-                  />
-                }
-                {<TVChartContainer {...widget} />}
-              </div>
+            <div className="h-[420px] rounded-lg text-center flex flex-col justify-center text-stieglitz">
+              <TVChart
+                data={gmxChartData}
+                triggerMarker={'0'}
+                period={period}
+                setPeriod={setPeriod}
+                colors={{
+                  backgroundColor: 'rgb(21, 21, 21)',
+                  lineColor: '#2962FF',
+                  textColor: 'white',
+                  areaTopColor: 'rgba(109, 255, 185, 0.2)',
+                  areaBottomColor: 'rgba(41, 98, 255, 0.1)',
+                }}
+              />
             </div>
             <div className="space-y-4">
               <StrikesChain selectedToken={selectedToken} />
