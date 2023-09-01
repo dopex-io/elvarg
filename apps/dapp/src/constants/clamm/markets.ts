c4e7522ba22c579a179970fa2ec498652b13b2c4
Aercwarden <arc@dopex.io>
2023-09-01 20:56:42 +0400

    feat: market type and data

--- apps/dapp/src/constants/clamm/markets.ts
+++ apps/dapp/src/constants/clamm/markets.ts
@@ -1,30 +1,42 @@
 import { Address } from 'viem';
 
+import {
+  ClammPair,
+  CollateralTokenTypes,
+  UnderlyingTokenTypes,
+} from 'store/Vault/clamm';
+
 interface ClamMarket {
-  underlyingSymbol: string;
+  underlyingTokenSymbol: UnderlyingTokenTypes;
+  collateralTokenSymbol: CollateralTokenTypes;
+  underlyingTokenAddress: Address;
   collateralTokenAddress: Address;
+  optionPools: ''; // Will update when contract is deployed
   uniswapPoolAddress: Address | undefined;
 }
 
-export const MARKETS: { [key: string]: ClamMarket } = {
-  ARB: {
-    underlyingSymbol: 'ARB',
-    collateralTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+export const MARKETS: Record<ClammPair, ClamMarket> = {
+  'ARB-USDC': {
+    underlyingTokenSymbol: 'ARB',
+    collateralTokenSymbol: 'USDC',
+    underlyingTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+    collateralTokenAddress: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
     uniswapPoolAddress: '0xcda53b1f66614552f834ceef361a8d12a0b8dad8',
+    optionPools: '',
   },
-  '42069inu': {
-    underlyingSymbol: '42069inu',
-    collateralTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
-    uniswapPoolAddress: undefined,
-  },
-  USDC: {
-    underlyingSymbol: 'USDC',
-    collateralTokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-    uniswapPoolAddress: undefined,
-  },
+  // '42069inu': {
+  //   underlyingSymbol: '42069inu',
+  //   collateralTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+  //   uniswapPoolAddress: undefined,
+  // },
+  // USDC: {
+  //   underlyingSymbol: 'USDC',
+  //   collateralTokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+  //   uniswapPoolAddress: undefined,
+  // },
 };
 
-export const FALLBACK_SLUG = '?tokenA=ARB';
+export const FALLBACK_SLUG = '?pair=ARB-USDC';
 
 export const MARKETS_MENU = Object.keys(MARKETS)
   .filter((token) => token !== 'USDC')
