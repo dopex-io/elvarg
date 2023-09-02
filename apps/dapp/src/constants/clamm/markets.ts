8427f8708aab5995f122405c3ff13004a8a07129
Aercwarden <arc@dopex.io>
2023-09-03 01:30:09 +0400

    feat: test addresses

--- apps/dapp/src/constants/clamm/markets.ts
+++ apps/dapp/src/constants/clamm/markets.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers';
 import { Address } from 'viem';
 
 import {
@@ -11,19 +12,40 @@ export interface ClammMarket {
   collateralTokenSymbol: CollateralTokenTypes;
   underlyingTokenAddress: Address;
   collateralTokenAddress: Address;
-  optionPools: ''; // Will update when contract is deployed
+  optionPool: Address;
   uniswapPoolAddress: Address;
 }
 
+export const positionManagerAddress =
+  '0x2a9a9f63F13dD70816C456B2f2553bb648EE0F8F';
+
+/// @TODO: checksum the addresses
 export const MARKETS: Record<ClammPair, ClammMarket> = {
   'ARB-USDC': {
     underlyingTokenSymbol: 'ARB',
     collateralTokenSymbol: 'USDC',
-    underlyingTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
-    collateralTokenAddress: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
-    uniswapPoolAddress: '0xcda53b1f66614552f834ceef361a8d12a0b8dad8',
-    optionPools: '',
+    underlyingTokenAddress: ethers.utils.getAddress(
+      '0x63a38a21f74e69f75209ae5a76dbe7f1558ab890',
+    ) as Address,
+    collateralTokenAddress: ethers.utils.getAddress(
+      '0x50dfff781e60875d304cac5f034d3f9e275445a9',
+    ) as Address,
+    uniswapPoolAddress: ethers.utils.getAddress(
+      '0xce0F8EfCa1Bc21Dd9AaEE6ee8F2c0F2155980bBB',
+    ) as Address,
+    optionPool: ethers.utils.getAddress(
+      '0xf41FF552dACA8a76D800cC3ABd368D75816F66fc',
+    ) as Address,
   },
+  // 'ARB-USDC': {
+  //   underlyingTokenSymbol: 'ARB',
+  //   collateralTokenSymbol: 'USDC',
+  //   underlyingTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+  //   collateralTokenAddress: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+  //   uniswapPoolAddress: '0xcda53b1f66614552f834ceef361a8d12a0b8dad8',
+  //   optionPools: '0x0',
+  // },
+
   // '42069inu': {
   //   underlyingSymbol: '42069inu',
   //   collateralTokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
