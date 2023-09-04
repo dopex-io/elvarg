d44500648215d30faf79668abad282e53c433566
garyunwin42 <garyunwin42@gmail.com>
2023-09-03 22:23:29 -0400

    feat: read pos from subgraph

--- apps/dapp/src/hooks/clamm/useClammPositions.ts
+++ apps/dapp/src/hooks/clamm/useClammPositions.ts
@@ -1,17 +1,25 @@
 import { useCallback, useEffect, useState } from 'react';
-import { formatUnits } from 'viem';
+import { formatEther } from 'viem';
 
 import request from 'graphql-request';
-import { Address, useAccount } from 'wagmi';
+import { Address } from 'wagmi';
 
 import queryClient from 'queryClient';
 
 import { getUserClammPositions } from 'graphql/clamm';
 
-import { ClammBuyPosition, ClammWritePosition } from 'store/Vault/clamm';
+import { useBoundStore } from 'store';
+import {
+  ClammBuyPosition,
+  ClammPair,
+  ClammWritePosition,
+} from 'store/Vault/clamm';
 
+import getPoolSlot0 from 'utils/clamm/getPoolSlot0';
+import splitMarketPair from 'utils/clamm/splitMarketPair';
+
+import { CHAINS } from 'constants/chains';
 import { MARKETS } from 'constants/clamm/markets';
-import { DECIMALS_TOKEN } from 'constants/index';
 import { DOPEX_CLAMM_SUBGRAPH_API_URL } from 'constants/subgraphs';
 
 export interface RewardsInfo {
@@ -40,7 +48,10 @@ interface Args {
 const useClammPositions = (args: Args) => {
   const { market } = args;
   //   const { address } = useAccount();
-  const address = '0x29ed22a9e56ee1813e6ff69fc6cac676aa24c09c';
+  const ARC = '0x2a9a9f63f13dd70816c456b2f2553bb648ee0f8f';
+  const CARROT = '0x29ed22a9e56ee1813e6ff69fc6cac676aa24c09c';
+  const address = CARROT;
+  const { selectedPair, chainId } = useBoundStore();
 
   const [writePositions, setWritePositions] = useState<ClammWritePosition[]>();
   const [buyPositions, setBuyPositions] = useState<ClammBuyPosition[]>();
@@ -48,7 +59,7 @@ const useClammPositions = (args: Args) => {
 
   const updateClammPositions = useCallback(async () => {
     setLoading(true);
-    if (!address || !MARKETS[market]) {
+    if (!address || MARKETS[market as ClammPair] === undefined) {
       setLoading(false);
       return;
     }
@@ -60,37 +71,46 @@ const useClammPositions = (args: Args) => {
         }),
     })) as any;
 
-    // TODO: confirm pool ID
-    const wantPoolId = MARKETS[market].uniswapPoolAddress;
+    const poolAddress = MARKETS[market as ClammPair].uniswapPoolAddress;
 
-    if (!queryResult.users[0] || !wantPoolId) {
+    if (!queryResult.users[0] || !poolAddress) {
       setLoading(false);
     }
 
-    // strikeSymbol: string;
-    // optionId: string;
-    // strike: number;
-    // tickLower: number;
-    // tickUpper: number;
-    // size: number;
-    // isPut: boolean;
+    const slot0 = await getPoolSlot0(poolAddress);
+    // @ts-ignore
+    const currentTick = slot0[1];
+
+    const { underlyingTokenSymbol, collateralTokenSymbol } =
+      splitMarketPair(selectedPair);
+
+    const decimals0 = CHAINS[chainId].tokenDecimals[underlyingTokenSymbol];
+    const decimals1 = CHAINS[chainId].tokenDecimals[collateralTokenSymbol];
+
     const _buyPositions: ClammBuyPosition[] =
       queryResult.users[0].userBuyPositions
         .filter((position) => {
           const poolId = position.id.split('#')[0] as string;
           return (
-            wantPoolId && poolId.toLowerCase() === wantPoolId.toLowerCase()
+            poolAddress && poolId.toLowerCase() === poolAddress.toLowerCase()
           );
         })
         .map((item) => {
+          const isPut = currentTick < item.lowerTick;
+          const strike = isPut
+            ? ((1 / 1.0001 ** item.tickLower) * 10 ** decimals0) /
+              10 ** decimals1
+            : ((1 / 1.0001 ** item.tickUpper) * 10 ** decimals0) /
+              10 ** decimals1;
           return {
-            balance: item.amount,
-            epoch: item.epoch,
-            strike: item.strike,
-            ssovAddress: item.ssov.id,
-            optionTokenAddress: '',
-            side: item.ssov.isPut ? 'Put' : 'Call',
-            premium: Number(formatUnits(item.premium, DECIMALS_TOKEN)),
+            strikeSymbol: market,
+            optionId: item.id,
+            strike: Number(strike),
+            tickLower: item.tickLower,
+            tickUpper: item.tickUpper,
+            size: item.options,
+            isPut: isPut,
+            expiry: item.expiry,
           };
         });
     setBuyPositions(_buyPositions);
@@ -100,19 +120,32 @@ const useClammPositions = (args: Args) => {
         .filter((position) => {
           const poolId = position.id.split('#')[0] as string;
           return (
-            wantPoolId && poolId.toLowerCase() === wantPoolId.toLowerCase()
+            poolAddress && poolId.toLowerCase() === poolAddress.toLowerCase()
           );
         })
         .map((item) => {
+          const isPut = currentTick < item.lowerTick;
+          const strike = isPut
+            ? ((1 / 1.0001 ** item.tickLower) * 10 ** decimals0) /
+              10 ** decimals1
+            : ((1 / 1.0001 ** item.tickUpper) * 10 ** decimals0) /
+              10 ** decimals1;
           return {
-            balance: item.amount,
-            epoch: item.epoch,
+            strikeSymbol: market,
+            optionId: item.id,
+            strike: Number(strike),
+            tickLower: item.tickLower,
+            tickUpper: item.tickUpper,
+            size: formatEther(item.size),
+            isPut: isPut,
+            earned: 0,
+            premiums: 0,
           };
         });
     setWritePositions(_writePositions);
 
     setLoading(false);
-  }, [address, market]);
+  }, [address, chainId, market, selectedPair]);
 
   useEffect(() => {
     updateClammPositions();
