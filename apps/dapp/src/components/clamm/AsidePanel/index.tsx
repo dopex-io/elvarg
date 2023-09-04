4713dcc1ee51211a85a0967302915f08811395e8
garyunwin42 <garyunwin42@gmail.com>
2023-09-03 22:34:14 -0400

    feat: default strike

--- apps/dapp/src/components/clamm/AsidePanel/index.tsx
+++ apps/dapp/src/components/clamm/AsidePanel/index.tsx
@@ -142,7 +142,6 @@ const AsidePanel = () => {
   const {
     updateSelectedStrike,
     selectedStrike,
-    clammStrikesData,
     isPut,
     positionManagerContract,
     optionPoolsContract,
@@ -236,8 +235,11 @@ const AsidePanel = () => {
 
   const loadStrikesForPair = useCallback(async () => {
     if (!uniswapPoolAddress) return console.error('UniswapPool not found');
-    setClammStrikes(await getStrikesWithTicks(10));
-  }, [uniswapPoolAddress, getStrikesWithTicks]);
+    const strikes = await getStrikesWithTicks(10);
+    const firstStrike = strikes[0];
+    setClammStrikes(strikes);
+    updateSelectedStrike(Number(firstStrike.strike.toFixed(5)));
+  }, [uniswapPoolAddress, getStrikesWithTicks, updateSelectedStrike]);
 
   // const debugAvailableLiquidity = useCallback(async () => {
   //   const clammStrike = clammStrikes.find(({ strike }) => {
