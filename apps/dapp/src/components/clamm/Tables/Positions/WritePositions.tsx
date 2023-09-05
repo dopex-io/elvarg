f0db24454ded87867201375daa883e45e366f5f7
Aercwarden <arc@dopex.io>
2023-09-05 08:34:31 +0400

    fix: data formatting and parsing

--- apps/dapp/src/components/clamm/Tables/Positions/WritePositions.tsx
+++ apps/dapp/src/components/clamm/Tables/Positions/WritePositions.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useMemo, useState } from 'react';
-import { parseEther } from 'viem';
+import { Address, formatUnits, parseEther } from 'viem';
 
 import { PositionsManager__factory } from '@dopex-io/sdk';
 import { Button } from '@dopex-io/ui';
@@ -13,10 +13,20 @@ import TableLayout from 'components/common/TableLayout';
 
 import { formatAmount } from 'utils/general';
 
+import { CHAINS } from 'constants/chains';
+
+import WithdrawModal from './WithdrawModal/WithdrawModal';
+
 interface WritePositionData {
   strikeSymbol: string;
   strike: string;
   size: string;
+  size2: {
+    token0: string;
+    token1: string;
+    token0Symbol: string;
+    token1Symbol: string;
+  };
   isPut: boolean;
   earnedAndPrice: {
     earned: string;
@@ -27,6 +37,12 @@ interface WritePositionData {
     premiums: string;
     price: number;
     tokenSymbol: string;
+  };
+  premium2: {
+    token0: string;
+    token1: string;
+    token0Symbol: string;
+    token1Symbol: string;
   };
   button: {
     handleBurn: () => void;
@@ -48,7 +64,26 @@ const columns = [
   }),
   columnHelper.accessor('size', {
     header: 'Size',
-    cell: (info) => <p>{info.getValue()}</p>,
+    cell: (info) => {
+      // @ts-ignore
+      const { token0, token1, token0Symbol, token1Symbol } = info.getValue();
+      console.log('token 0 type', Number(token0) > 0);
+      console.log('token 1 type', Number(token1) > 0);
+      return (
+        <div className="flex flex-col items-start justify-center">
+          {Number(token0) !== 0 && (
+            <span>
+              {token0} <span className="text-stieglitz">{token0Symbol}</span>
+            </span>
+          )}
+          {Number(token1) !== 0 && (
+            <span>
+              {token1} <span className="text-stieglitz">{token1Symbol}</span>
+            </span>
+          )}
+        </div>
+      );
+    },
   }),
   columnHelper.accessor('isPut', {
     header: 'Side',
@@ -69,6 +104,24 @@ const columns = [
             {value.premiums} {value.tokenSymbol}
           </p>
         </>
+      );
+    },
+  }),
+  columnHelper.accessor('premium2', {
+    header: 'Premiums',
+    cell: (info) => {
+      const { token0, token0Symbol, token1, token1Symbol } = info.getValue();
+      return (
+        <div className="flex flex-col items-start justify-center">
+          {
+            <span>
+              {token0} <span className="text-stieglitz">{token0Symbol}</span>
+            </span>
+          }
+          <span>
+            {token1} <span className="text-stieglitz">{token1Symbol}</span>
+          </span>
+        </div>
       );
     },
   }),
@@ -103,19 +156,53 @@ const columns = [
   }),
 ];
 
-type BurnPositionProps = Pick<
-  ClammWritePosition,
-  'tickLower' | 'tickUpper' | 'size'
->;
+type BurnPositionProps = {
+  tickLower: number;
+  tickUpper: number;
+  shares: bigint;
+};
 
 const WritePositions = ({
   writePositions,
 }: {
   writePositions: ClammWritePosition[] | undefined;
 }) => {
-  const { positionManagerContract, uniswapPoolContract, clammMarkPrice } =
-    useBoundStore();
+  const {
+    positionManagerContract,
+    uniswapPoolContract,
+    clammMarkPrice,
+    selectedUniswapPool,
+    chainId,
+  } = useBoundStore();
   const [selectedPosition, setSelectedPosition] = useState<BurnPositionProps>();
+
+  const tokenDecimalsAndSymbols = useMemo(() => {
+    const amount0Symbol =
+      selectedUniswapPool.token0 == selectedUniswapPool.underlyingToken
+        ? selectedUniswapPool.underlyingTokenSymbol
+        : selectedUniswapPool.collateralTokenSymbol;
+    const amount1Symbol =
+      selectedUniswapPool.token1 == selectedUniswapPool.collateralTokenSymbol
+        ? selectedUniswapPool.collateralTokenSymbol
+        : selectedUniswapPool.underlyingTokenSymbol;
+
+    const amount0Decimals = CHAINS[chainId].tokenDecimals[amount0Symbol];
+    const amount1Decimals = CHAINS[chainId].tokenDecimals[amount1Symbol];
+
+    return {
+      amount0Decimals,
+      amount1Decimals,
+      amount0Symbol,
+      amount1Symbol,
+    };
+  }, [
+    chainId,
+    selectedUniswapPool.collateralTokenSymbol,
+    selectedUniswapPool.token0,
+    selectedUniswapPool.token1,
+    selectedUniswapPool.underlyingToken,
+    selectedUniswapPool.underlyingTokenSymbol,
+  ]);
 
   const { config: burnPositionConfig } = usePrepareContractWrite({
     abi: PositionsManager__factory.abi,
@@ -126,19 +213,26 @@ const WritePositions = ({
         pool: uniswapPoolContract,
         tickLower: selectedPosition?.tickLower || 0,
         tickUpper: selectedPosition?.tickUpper || 0,
-        shares: parseEther((selectedPosition?.size || 0).toString()),
+        shares: 1n,
       },
     ],
   });
   const { write: burnPosition } = useContractWrite(burnPositionConfig);
 
   const handleBurn = useCallback(
-    (index: number) => {
+    (index: number, withdrawable: bigint) => {
       if (!writePositions) return;
+
+      console.log(
+        'parameters',
+        writePositions[index].tickLower,
+        writePositions[index].tickUpper,
+        writePositions[index].balance,
+      );
       setSelectedPosition({
         tickLower: writePositions[index].tickLower,
         tickUpper: writePositions[index].tickUpper,
-        size: writePositions[index].size,
+        shares: withdrawable - 10000000000000n,
       });
       burnPosition?.();
     },
@@ -151,7 +245,25 @@ const WritePositions = ({
       return {
         strikeSymbol: position.strikeSymbol,
         strike: formatAmount(position.strike, 3),
-        size: formatAmount(Number(position.size), 3),
+        // size: formatAmount(Number(position.size), 3),
+        size: {
+          token0: formatAmount(
+            formatUnits(
+              position.size.token0,
+              tokenDecimalsAndSymbols.amount0Decimals,
+            ),
+            4,
+          ),
+          token0Symbol: tokenDecimalsAndSymbols.amount0Symbol,
+          token1: formatAmount(
+            formatUnits(
+              position.size.token1,
+              tokenDecimalsAndSymbols.amount1Decimals,
+            ),
+            4,
+          ),
+          token1Symbol: tokenDecimalsAndSymbols.amount1Symbol,
+        },
         isPut: position.isPut,
         earnedAndPrice: {
           earned: formatAmount(position.earned, 3),
@@ -163,21 +275,50 @@ const WritePositions = ({
           price: clammMarkPrice,
           tokenSymbol: position.strikeSymbol.split('-')[0],
         },
+        premium2: {
+          token0: formatAmount(
+            formatUnits(
+              position.premiums2.token0,
+              tokenDecimalsAndSymbols.amount1Decimals,
+            ),
+            4,
+          ),
+          token0Symbol: tokenDecimalsAndSymbols.amount0Symbol,
+          token1: formatAmount(
+            formatUnits(
+              position.premiums2.token1,
+              tokenDecimalsAndSymbols.amount1Decimals,
+            ),
+            4,
+          ),
+          token1Symbol: tokenDecimalsAndSymbols.amount1Symbol,
+        },
         button: {
-          handleBurn: () => handleBurn(index),
+          handleBurn: () => handleBurn(index, position.withdrawable),
           id: index,
         },
       };
     });
-  }, [writePositions, clammMarkPrice, handleBurn]);
+  }, [
+    writePositions,
+    clammMarkPrice,
+    handleBurn,
+    tokenDecimalsAndSymbols.amount0Decimals,
+    tokenDecimalsAndSymbols.amount1Decimals,
+    tokenDecimalsAndSymbols.amount0Symbol,
+    tokenDecimalsAndSymbols.amount1Symbol,
+  ]);
 
   return (
-    <TableLayout<WritePositionData>
-      data={positions}
-      columns={columns}
-      rowSpacing={2}
-      isContentLoading={false}
-    />
+    <div>
+      <WithdrawModal open={true} handleClose={() => 0} />
+      <TableLayout<WritePositionData>
+        data={positions}
+        columns={columns}
+        rowSpacing={2}
+        isContentLoading={false}
+      />
+    </div>
   );
 };
 
