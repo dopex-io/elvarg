e8f7fd4786ef4d8405456e610601ced4d1ba29aa
garyunwin42 <garyunwin42@gmail.com>
2023-09-06 14:03:42 -0400

    fix: dialog

--- apps/dapp/src/components/clamm/AsidePanel/DisclaimerDialog.tsx
+++ apps/dapp/src/components/clamm/AsidePanel/DisclaimerDialog.tsx
@@ -6,36 +6,37 @@ const DisclaimerDialog = (props: any) => {
   const { isOpen, handleClose, handleAgree } = props;
 
   return (
-    <Dialog
-      title="Deposit"
-      isOpen={isOpen}
-      handleClose={handleClose}
-      showCloseIcon
-    >
-      <div>
-        <div className="p-3">
-          <p className="text-white font-semibold pb-2">CLAMM user agreement</p>
-          <p className="text-stieglitz">
-            By using the Dopex CLAMM Beta product, I agree to the following
-            terms and conditions:
-          </p>
-        </div>
-        <div className="p-3 bg-umbra rounded-lg">
-          {CLAMM_DISCLAIMER_MESSAGE['english']
-            .split('\n')
-            .map((message, index) => (
-              <p className="py-2 px-1 text-white" key={index}>
-                &#x2022; {message}
-              </p>
-            ))}
-          <div className="pt-4 pb-1 w-full flex items-center justify-center">
-            <Button onClick={handleAgree} className="w-full px-5">
-              I agree
-            </Button>
+    <div className="mt-20 z-40">
+      <Dialog
+        title="CLAMM user agreement"
+        isOpen={isOpen}
+        handleClose={handleClose}
+        showCloseIcon
+      >
+        <div>
+          <div className="p-3">
+            <p className="text-stieglitz">
+              By using the Dopex CLAMM Beta product, I agree to the following
+              terms and conditions:
+            </p>
+          </div>
+          <div className="p-3 bg-umbra rounded-lg">
+            {CLAMM_DISCLAIMER_MESSAGE['english']
+              .split('\n')
+              .map((message, index) => (
+                <p className="py-2 px-1 text-white text-sm" key={index}>
+                  &#x2022; {message}
+                </p>
+              ))}
+            <div className="pt-4 pb-1 w-full flex items-center justify-center">
+              <Button onClick={handleAgree} className="w-full px-5">
+                I agree
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </Dialog>
+      </Dialog>
+    </div>
   );
 };
 
