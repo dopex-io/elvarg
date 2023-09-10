acd4768f06cfd59cfefcd0a38840759ea382d5fb
Aercwarden <arc@dopex.io>
2023-09-10 18:28:51 +0400

    feat: history rework

---
+++ apps/dapp/src/components/clamm/Tables/Positions/TradeHistory.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+
+import { createColumnHelper } from '@tanstack/react-table';
+import { formatDistance } from 'date-fns';
+
+import TableLayout from 'components/common/TableLayout';
+
+import { formatAmount } from 'utils/general';
+
+type TradeHistoryData = {
+  status: string;
+  tickLower: number;
+  tickUpper: number;
+  exercised: boolean;
+  pnl: {
+    amount: string;
+    symbol: string;
+    usdValue: number;
+  };
+  strike: number;
+  side: string;
+  size: {
+    amount: string;
+    symbol: string;
+  };
+  expiry: number;
+  exercisableAmount: bigint;
+};
+
+const columnHelper = createColumnHelper<TradeHistoryData>();
+const columns = [
+  columnHelper.accessor('strike', {
+    header: 'Strike Price',
+    cell: ({ getValue }) => (
+      <span className="space-x-2 text-left">
+        <p className="text-stieglitz inline-block">$</p>
+        <p className="inline-block">{getValue().toFixed(5)}</p>
+      </span>
+    ),
+  }),
+  columnHelper.accessor('size', {
+    header: 'Size',
+    cell: ({ getValue }) => (
+      <p>
+        {formatAmount(getValue().amount, 5)}{' '}
+        <span className="text-stieglitz">{getValue().symbol}</span>
+      </p>
+    ),
+  }),
+  columnHelper.accessor('expiry', {
+    header: 'Expiry',
+    cell: (info) => (
+      <p className="overflow-hidden whitespace-nowrap">
+        {formatDistance(Number(info.getValue()) * 1000, new Date())}{' '}
+        {Number(info.getValue()) * 1000 < new Date().getTime() && 'ago'}
+      </p>
+    ),
+  }),
+  // columnHelper.accessor('pnl', {
+  //   header: 'PnL',
+  //   cell: (info) => {
+  //     let { amount, symbol } = info.getValue();
+  //     const amountInNumber = Number(amount);
+
+  //     return (
+  //       <>
+  //         <span className="space-x-2">
+  //           {Number(amountInNumber) === 0 && (
+  //             <p className="text-stieglitz inline-block">
+  //               {formatAmount(amountInNumber, 5)}
+  //             </p>
+  //           )}
+  //           {Number(amountInNumber) > 0 && (
+  //             <p className="text-up-only inline-block">
+  //               {formatAmount(amountInNumber, 5)}
+  //             </p>
+  //           )}
+  //           {Number(amountInNumber) < 0 && (
+  //             <p className="text-down-bad inline-block">
+  //               {formatAmount(amountInNumber, 5)}
+  //             </p>
+  //           )}
+  //           <p className="text-stieglitz inline-block">{symbol}</p>
+  //         </span>
+  //         {/* <p className="text-stieglitz">${formatAmount(usdValue, 5)}</p> */}
+  //       </>
+  //     );
+  //   },
+  // }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    cell: ({ getValue }) => (
+      <p>
+        <span className="text-stieglitz">{getValue()}</span>
+      </p>
+    ),
+  }),
+];
+
+const TradeHistory = ({
+  tradeHistory,
+}: {
+  tradeHistory: TradeHistoryData[];
+}) => {
+  return (
+    <div>
+      <TableLayout<TradeHistoryData>
+        data={tradeHistory}
+        columns={columns}
+        rowSpacing={3}
+        isContentLoading={false}
+      />
+    </div>
+  );
+};
+
+export default TradeHistory;
--- apps/dapp/src/components/clamm/Tables/Positions/index.tsx
+++ apps/dapp/src/components/clamm/Tables/Positions/index.tsx
@@ -7,8 +7,8 @@ import { useBoundStore } from 'store';
 
 import { ButtonGroup } from 'components/clamm/AsidePanel';
 
-import HistoryPositions from './HistoryPositions';
 import OptionsPositions from './OptionsPositions';
+import TradeHistory from './TradeHistory';
 import WritePositions from './WritePositions';
 
 const Positions = () => {
@@ -187,85 +187,27 @@ const Positions = () => {
     writePositions,
   ]);
 
-  const historyPositions = useMemo(() => {
-    if (!optionsPool) return [];
+  const tradeHistory = useMemo(() => {
+    const currentTimestamp = Number((new Date().getTime() / 1000).toFixed(0));
+    const positions = filteredOptionsPositions.filter(({ expiry }) => {
+      return expiry < currentTimestamp;
+    });
 
-    let positions = optionsPositions.map(
-      ({
-        callOrPut,
-        tickLowerPrice,
-        tickUpperPrice,
-        amounts,
-        expiry,
-        exercisableAmount,
-        exercised,
-        tickLower,
-        tickUpper,
-      }) => {
-        const sizeAmount = formatUnits(
-          amounts[callOrPut ? keys.callAssetAmountKey : keys.putAssetAmountKey],
-          optionsPool[
-            callOrPut ? keys.callAssetDecimalsKey : keys.putAssetDecimalsKey
-          ],
-        );
-        let size = {
-          amount: sizeAmount,
-          symbol:
-            optionsPool[
-              callOrPut ? keys.callAssetSymbolKey : keys.putAssetSymbolKey
-            ],
-        };
-
-        let _pnl =
-          (callOrPut
-            ? markPrice - tickUpperPrice
-            : tickLowerPrice - markPrice) * Number(sizeAmount);
-
-        let usdValue = _pnl;
-        _pnl = callOrPut ? _pnl : _pnl / markPrice;
-
-        if (_pnl < 0) {
-          _pnl = 0;
-          usdValue = 0;
-        }
-
-        const pnl = {
-          amount: _pnl.toString(),
-          symbol:
-            optionsPool[
-              callOrPut ? keys.putAssetSymbolKey : keys.callAssetSymbolKey
-            ],
-          usdValue: usdValue,
-        };
-
-        return {
-          tickLower,
-          tickUpper,
-          exercised,
-          pnl,
-          strike: callOrPut ? tickUpperPrice : tickLowerPrice,
-          side: callOrPut ? 'Call' : 'Put',
-          size,
-          expiry,
-          exercisableAmount,
-        };
-      },
-    );
-
-    positions = positions.filter(({ exercised }) => exercised === true);
-    positions = positions.sort((a, b) => b.expiry - a.expiry);
-    return positions;
-  }, [
-    keys.callAssetAmountKey,
-    keys.callAssetDecimalsKey,
-    keys.callAssetSymbolKey,
-    keys.putAssetAmountKey,
-    keys.putAssetDecimalsKey,
-    keys.putAssetSymbolKey,
-    markPrice,
-    optionsPool,
-    optionsPositions,
-  ]);
+    const filteredPositions = positions.map((position) => {
+      const exercised = 'Exercised';
+      const expired = 'Expired';
+      const status = position.exercisableAmount < 10n ? exercised : expired;
+      return {
+        ...position,
+        status,
+        pnl: {
+          ...position.pnl,
+          amount: status === expired ? '0' : position.pnl.amount,
+        },
+      };
+    });
+    return filteredPositions;
+  }, [filteredOptionsPositions]);
 
   const buttonLabels = useMemo(() => {
     return [
@@ -282,17 +224,10 @@ const Positions = () => {
         </div>
       </div>,
       <div className="flex space-x-2 my-auto" key="buy-positions">
-        <span>Trade History</span>
-        <div className="rounded-full bg-carbon w-5 h-auto flex items-center justify-center">
-          <span>{historyPositions.length}</span>
-        </div>
+        <span>History</span>
       </div>,
     ];
-  }, [
-    filteredOptionsPositions.length,
-    filteredWritePositions.length,
-    historyPositions.length,
-  ]);
+  }, [filteredOptionsPositions, filteredWritePositions]);
 
   const handleClick = (index: number) => {
     setActiveIndex(index);
@@ -319,13 +254,13 @@ const Positions = () => {
       return <OptionsPositions optionsPositions={filteredOptionsPositions} />;
     else if (activeIndex === 1)
       return <WritePositions writePositions={filteredWritePositions} />;
-    return <HistoryPositions historyPositions={historyPositions} />;
+    return <TradeHistory tradeHistory={tradeHistory} />;
   }, [
     loading.positions,
     activeIndex,
+    tradeHistory,
     filteredOptionsPositions,
     filteredWritePositions,
-    historyPositions,
   ]);
 
   return (
