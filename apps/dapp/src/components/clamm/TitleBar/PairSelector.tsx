25a2351024d0b0e164ffedfd8a2098ab80ea9664
Aercwarden <arc@dopex.io>
2023-09-22 05:15:49 +0400

    feat: stats

--- apps/dapp/src/components/clamm/TitleBar/PairSelector.tsx
+++ apps/dapp/src/components/clamm/TitleBar/PairSelector.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { formatUnits } from 'viem';
 
 import { Menu } from '@dopex-io/ui';
 
@@ -6,13 +7,22 @@ import { useBoundStore } from 'store';
 
 import TitleItem from 'components/ssov-beta/TitleBar/TitleItem';
 
+import { formatAmount } from 'utils/general';
+
 import {
   CLAMM_COLLATERAL_TOKENS_LIST,
   CLAMM_UNDERLYING_TOKENS_LIST,
 } from 'constants/clamm';
 
 export function PairSelector() {
-  const { setSelectedOptionsPoolPair, markPrice } = useBoundStore();
+  const {
+    setSelectedOptionsPoolPair,
+    markPrice,
+    ticksData,
+    optionsPool,
+    keys,
+    tokenPrices,
+  } = useBoundStore();
 
   const [underlyingToken, setUnderlyingToken] = useState<string>('ARB');
   const [collateralToken, setCollateralToken] = useState<string>('USDC');
@@ -34,6 +44,88 @@ export function PairSelector() {
     },
     [setSelectedOptionsPoolPair, underlyingToken],
   );
+
+  const stats = useMemo(() => {
+    if (!optionsPool)
+      return {
+        TVL: 0,
+        openInterest: 0,
+      };
+    const putAssetSymbol = optionsPool[keys.putAssetSymbolKey];
+    const callAssetSymbol = optionsPool[keys.callAssetSymbolKey];
+
+    const putAssetPrice =
+      tokenPrices.find(
+        (data) => data.name.toLowerCase() === putAssetSymbol.toLowerCase(),
+      )?.price ?? 0;
+    const callAssetPrice =
+      tokenPrices.find(
+        (data) => data.name.toLowerCase() === callAssetSymbol.toLowerCase(),
+      )?.price ?? 0;
+
+    console.log(tokenPrices);
+
+    let TVL = 0;
+    let openInterest = 0;
+
+    for (let i = 0; i < ticksData.length; i++) {
+      // ticksData[i].totalLiquidity;
+      const callAssetDecimals = optionsPool[keys.callAssetDecimalsKey];
+      const putAssetDecimals = optionsPool[keys.putAssetDecimalsKey];
+
+      const callAssetsValue =
+        Number(
+          formatUnits(
+            ticksData[i].totalLiquidity[keys.callAssetAmountKey],
+            callAssetDecimals,
+          ),
+        ) * callAssetPrice;
+      const putAssetsValue =
+        Number(
+          formatUnits(
+            ticksData[i].totalLiquidity[keys.putAssetAmountKey],
+            putAssetDecimals,
+          ),
+        ) * putAssetPrice;
+
+      TVL += callAssetsValue + putAssetsValue;
+
+      const callAssetsUsedValue =
+        Number(
+          formatUnits(
+            ticksData[i].totalLiquidity[keys.callAssetAmountKey] -
+              ticksData[i].liquidityAvailable[keys.callAssetAmountKey],
+            callAssetDecimals,
+          ),
+        ) * callAssetPrice;
+
+      const putAssetsUsedValue =
+        Number(
+          formatUnits(
+            ticksData[i].totalLiquidity[keys.putAssetAmountKey] -
+              ticksData[i].liquidityAvailable[keys.putAssetAmountKey],
+            putAssetDecimals,
+          ),
+        ) * putAssetPrice;
+
+      openInterest += callAssetsUsedValue + putAssetsUsedValue;
+    }
+
+    return {
+      TVL,
+      openInterest,
+    };
+  }, [
+    keys.callAssetAmountKey,
+    keys.callAssetDecimalsKey,
+    keys.callAssetSymbolKey,
+    keys.putAssetAmountKey,
+    keys.putAssetDecimalsKey,
+    keys.putAssetSymbolKey,
+    optionsPool,
+    ticksData,
+    tokenPrices,
+  ]);
 
   return (
     <>
@@ -75,18 +167,18 @@ export function PairSelector() {
           label="Mark Price"
           value={markPrice === 0 ? '...' : markPrice.toFixed(5)}
         />
-        {/* <TitleItem
+        <TitleItem
           symbol="$"
           symbolPrefixed
           label="Open Interest"
-          value={formatAmount(openInterest, 3, true)}
+          value={formatAmount(stats.openInterest, 3, true)}
         />
         <TitleItem
           symbol="$"
           symbolPrefixed
-          label="Total Volume"
-          value={formatAmount(totalVolume, 3, true)}
-        /> */}
+          label="Total Deposits"
+          value={formatAmount(stats.TVL, 3, true)}
+        />
       </div>
     </>
   );
