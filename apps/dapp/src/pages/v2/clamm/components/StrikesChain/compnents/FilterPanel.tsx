edb3b1b194f8a708254555546d183b17ad9891c7
Aercwarden <arc@dopex.io>
2023-10-21 10:51:33 +0400

    feat: filter panel

---
+++ apps/dapp/src/pages/v2/clamm/components/StrikesChain/compnents/FilterPanel.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import cx from 'classnames';
+
+const FilterPanel = () => {
+  const [isTrade, setIsTrade] = useState(false);
+  const [isPut, setIsPut] = useState(false);
+
+  return (
+    <div className="m-[12px] h-[30px] flex items-center justify-center space-x-[12px] w-fit">
+      <div className="rounded-md h-[32px] bg-mineshaft flex items-center justify-center space-x-[4px] p-[4px]">
+        <div
+          role="button"
+          onClick={() => setIsPut(false)}
+          className={`h-full flex items-center justify-center`}
+        >
+          <span
+            className={cx(
+              !isPut && 'bg-carbon',
+              'p-[4px] rounded-md font-medium text-[13px]',
+            )}
+          >
+            Calls
+          </span>
+        </div>
+        <div
+          role="button"
+          onClick={() => setIsPut(true)}
+          className="h-full flex items-center justify-center"
+        >
+          <span
+            className={cx(
+              isPut && 'bg-carbon',
+              'p-[4px] rounded-md font-medium text-[13px]',
+            )}
+          >
+            Puts
+          </span>
+        </div>
+      </div>
+      <div className="rounded-md h-[32px] bg-mineshaft flex items-center justify-center space-x-[4px] p-[4px]">
+        <div
+          role="button"
+          onClick={() => setIsTrade(true)}
+          className="h-full flex items-center justify-center"
+        >
+          <span
+            className={cx(
+              isTrade && 'bg-carbon',
+              'p-[4px] rounded-md font-medium text-[13px]',
+            )}
+          >
+            Trade
+          </span>
+        </div>
+        <div
+          role="button"
+          onClick={() => setIsTrade(false)}
+          className="h-full flex items-center justify-center"
+        >
+          <span
+            className={cx(
+              'p-[4px] rounded-md text-[13px] font-medium',
+              !isTrade && 'bg-carbon',
+            )}
+          >
+            Liquidity Provision
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterPanel;
