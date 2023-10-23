e52c55b459ba6a13dcfaccfc1c5d1112784076c5
Aercwarden <arc@dopex.io>
2023-10-24 01:06:09 +0400

    feat: strikes chain store

--- apps/dapp/src/hooks/clamm/useStrikesChainStore.tsx
+++ apps/dapp/src/hooks/clamm/useStrikesChainStore.tsx
@@ -1,14 +1,25 @@
+import { StrikesChainAPIResponse } from 'pages/v2/clamm/utils/varrock/getStrikesChain';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+type StrikesChain = StrikesChainAPIResponse;
 export interface StrikesChainStore {
   selectedStrikes: Map<number, number>;
-  selectStrike: Function;
-  deselectStrike: Function;
-  toggleSelect: Function;
+  selectStrike: (index: number, strike: any) => void;
+  deselectStrike: (index: number) => void;
+  initialize: (data: StrikesChainAPIResponse) => void;
+  strikesChain: StrikesChain;
 }
 
 const useStrikesChainStore = create<StrikesChainStore>((set) => ({
+  strikesChain: [],
+  initialize: (data: StrikesChain) => {
+    console.log(data);
+    set((prev) => ({
+      ...prev,
+      strikesChain: data,
+    }));
+  },
   selectedStrikes: new Map(),
   selectStrike: (index: number, strike: number) => {
     set((prev) => {
@@ -25,20 +36,6 @@ const useStrikesChainStore = create<StrikesChainStore>((set) => ({
       const selectedStrikes = new Map(prev.selectedStrikes);
       selectedStrikes.delete(index);
 
-      return {
-        ...prev,
-        selectedStrikes: selectedStrikes,
-      };
-    });
-  },
-  toggleSelect: (index: number, strike: number) => {
-    set((prev) => {
-      const selectedStrikes = new Map(prev.selectedStrikes);
-      if (selectedStrikes.has(index)) {
-        selectedStrikes.delete(index);
-      } else {
-        selectedStrikes.set(index, strike);
-      }
       return {
         ...prev,
         selectedStrikes: selectedStrikes,
