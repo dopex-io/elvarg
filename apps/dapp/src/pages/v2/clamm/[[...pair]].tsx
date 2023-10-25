a9bf7761128861812dc127b1ec662a58499f02b9
Aercwarden <arc@dopex.io>
2023-10-25 14:52:02 +0400

    feat: responsiveness and use query

--- apps/dapp/src/pages/v2/clamm/[[...pair]].tsx
+++ apps/dapp/src/pages/v2/clamm/[[...pair]].tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 
 import { Button } from '@dopex-io/ui';
+import { useQuery } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNetwork } from 'wagmi';
 
@@ -17,28 +18,49 @@ import PriceChartWithHide from './components/PriceChartWithHide';
 import StrikesChain from './components/StrikesChain';
 import OverViewStats from './components/TitleBar/OverViewStats';
 import PairSelector from './components/TitleBar/PairSelector';
-import getOptionsPools from './utils/varrock/getOptionsPools';
+import { VARROACK_BASE_API_URL } from './constants';
+
+const MAX_AFK_LIMIT = 60000;
 
 const Page = () => {
   const { initialize } = useClammStore();
-  const { setLoading, isLoading } = useLoadingStates();
   const { chain } = useNetwork();
 
+  const { data, refetch, dataUpdatedAt } = useQuery({
+    queryKey: ['clamm-pools-' + chain?.id],
+    initialData: [],
+    staleTime: 0,
+    queryFn: () => {
+      const queryUrl = new URL(`${VARROACK_BASE_API_URL}/clamm/pools`);
+      queryUrl.searchParams.set('chainId', chain?.id.toString() ?? '42161');
+      return fetch(queryUrl).then((res) => res.json());
+    },
+  });
+
   useEffect(() => {
-    setLoading('initial_pool_load', true);
-    getOptionsPools(chain?.id ?? 42161, initialize, (error) => {
-      toast.error(error);
-    });
-    setLoading('initial_pool_load', false);
-  }, [initialize, setLoading, isLoading, chain]);
+    if (data.error) {
+      toast.error('Failed to load pools from API.');
+      return;
+    }
+    initialize(data);
+  }, [data, initialize]);
+
+  // useEffect(() => {
+  //   window.addEventListener('visibilitychange', () => {
+  //     if (dataUpdatedAt + MAX_AFK_LIMIT < new Date().getTime()) {
+  //       refetch();
+  //     }
+  //   });
+  //   return () => window.removeEventListener('visibilitychange', () => {});
+  // });
 
   return (
     <PageLayout>
-      <div className="flex flex-col xl:flex-row w-full xl:items-start items-center justify-center 2xl:space-x-[12px] xl:space-x-[12px] space-y-[12px] 2xl:space-y-[0px]">
+      <div className="flex flex-col xl:flex-row w-full xl:items-start items-center justify-center 2xl:space-x-[12px] xl:space-x-[12px] space-y-[12px] 2xl:space-y-[0px] lg:mb-4">
         <div
           className={`flex flex-col 2xl:w-[1314px] xl:w-[900px] w-[90vw] space-y-[12px] h-full`}
         >
-          <div className="flex flex-col sm:flex-row w-full sm:items-end h-full space-y-[24px] sm:space-y-[0px] sm:space-x-[24px]">
+          <div className="flex flex-col md:flex-row w-full md:items-end h-full space-y-[24px] md:space-y-[0px] md:space-x-[24px]">
             <PairSelector />
             <OverViewStats />
           </div>
@@ -47,7 +69,7 @@ const Page = () => {
           <PositionsTable />
         </div>
         {/* convert to dialog */}
-        <div className="xl:hidden sticky bottom-0 h-full w-full z-10 bg-cod-gray flex px-[24px] justify-around space-x-4 py-5 rounded-lg">
+        <div className="xl:hidden sticky bottom-0 w-full z-10 bg-cod-gray flex px-[24px] justify-around space-x-4 py-5 rounded-lg">
           <Button className="flex-1">Trade</Button>
           <Button className="flex-1">Liquidity Provision</Button>
         </div>
