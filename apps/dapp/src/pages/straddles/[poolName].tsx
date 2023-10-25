15a28d73cc0111f092740305bd9a054300bb37e0
WitherBlock <witherblock@protonmail.com>
2023-10-25 15:28:37 +0400

    fix: straddles purchase tab broken

--- apps/dapp/src/pages/straddles/[poolName].tsx
+++ apps/dapp/src/pages/straddles/[poolName].tsx
@@ -1,24 +1,17 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-import Box from '@mui/material/Box';
-
 import { NextSeo } from 'next-seo';
+
 import { useBoundStore } from 'store';
 
-import Typography from 'components/UI/Typography';
 import AppBar from 'components/common/AppBar';
-import PoolCard from 'components/straddles/Charts/PoolCard';
-import TVLCard from 'components/straddles/Charts/TVLCard';
 import Deposits from 'components/straddles/Deposits';
-import Manage from 'components/straddles/Manage';
 import Positions from 'components/straddles/Positions';
 import Stats from 'components/straddles/Stats';
 import TopBar from 'components/straddles/TopBar';
 
 import seo from 'constants/seo';
-
-const SHOWCHARTS = false;
 
 const Straddles = ({ poolName }: { poolName: string }) => {
   const {
@@ -39,7 +32,7 @@ const Straddles = ({ poolName }: { poolName: string }) => {
           updateStraddlesUserData().then(() => {
             setIsLoading(false);
           });
-        })
+        }),
       );
     }
   }, [
@@ -53,54 +46,26 @@ const Straddles = ({ poolName }: { poolName: string }) => {
   ]);
 
   return (
-    <Box className="bg-black min-h-screen">
+    <div className="bg-black min-h-screen">
       <AppBar />
-      <Box className="md:flex pt-5">
-        <Box className="ml-auto lg:w-[45%]">
-          <Box className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
+      <div className="md:flex pt-5">
+        <div className="mx-auto lg:w-[45%]">
+          <div className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
             <TopBar />
-          </Box>
-          <Box className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
+          </div>
+          <div className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
             <Stats />
-          </Box>
-          {SHOWCHARTS ? (
-            <Box>
-              <Box className="pt-8 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0">
-                <Typography variant="h6" className="-ml-1">
-                  Liquidity
-                </Typography>
-              </Box>
-              <Box className="pt-4 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 relative md:flex">
-                <Typography
-                  variant="h4"
-                  className="md:left-[40%] left-[25%] top-[40%] absolute"
-                >
-                  <span className="text-white">Not available yet</span>
-                </Typography>
-                <Box className="md:w-1/2 w-full md:pr-2">
-                  <PoolCard />
-                </Box>
-                <Box className="md:w-1/2 w-full md:pl-2">
-                  <TVLCard />
-                </Box>
-              </Box>
-            </Box>
-          ) : null}
-
-          <Box className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 mt-5">
-            <Typography variant="h6" className="-ml-1">
-              Deposits
-            </Typography>
-          </Box>
-          <Box className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
+          </div>
+          <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 mt-5">
+            <div className="-ml-1 text-sm">Deposits</div>
+          </div>
+          <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
             <Deposits />
-          </Box>
-          <Box className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0">
-            <Typography variant="h6" className="-ml-1">
-              Positions
-            </Typography>
-          </Box>
-          <Box className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
+          </div>
+          <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0">
+            <div className="-ml-1 text-sm">Positions</div>
+          </div>
+          <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
             <Positions />
             {chainId === 137 ? (
               <img
@@ -109,13 +74,10 @@ const Straddles = ({ poolName }: { poolName: string }) => {
                 alt="Powered by Polygon"
               />
             ) : null}
-          </Box>
-        </Box>
-        <Box className="lg:pt-32 sm:pt-20 lg:mr-auto md:mx-0 mx-4 mb-8 px-2 lg:px-0 lg:ml-32">
-          <Manage />
-        </Box>
-      </Box>
-    </Box>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
