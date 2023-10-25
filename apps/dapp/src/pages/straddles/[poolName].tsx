016eb57f994988a350bf04cac86ac708f4499719
WitherBlock <witherblock@protonmail.com>
2023-10-25 15:56:15 +0400

    chore: add close down banner

--- apps/dapp/src/pages/straddles/[poolName].tsx
+++ apps/dapp/src/pages/straddles/[poolName].tsx
@@ -6,6 +6,7 @@ import { NextSeo } from 'next-seo';
 import { useBoundStore } from 'store';
 
 import AppBar from 'components/common/AppBar';
+import StraddleClosedBanner from 'components/common/Banners/StraddlesClosedBanner';
 import Deposits from 'components/straddles/Deposits';
 import Positions from 'components/straddles/Positions';
 import Stats from 'components/straddles/Stats';
@@ -48,33 +49,32 @@ const Straddles = ({ poolName }: { poolName: string }) => {
   return (
     <div className="bg-black min-h-screen">
       <AppBar />
-      <div className="md:flex pt-5">
-        <div className="mx-auto lg:w-[45%]">
-          <div className="lg:pt-28 sm:pt-20 pt-20 lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
-            <TopBar />
-          </div>
-          <div className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
-            <Stats />
-          </div>
-          <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 mt-5">
-            <div className="-ml-1 text-sm">Deposits</div>
-          </div>
-          <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
-            <Deposits />
-          </div>
-          <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0">
-            <div className="-ml-1 text-sm">Positions</div>
-          </div>
-          <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
-            <Positions />
-            {chainId === 137 ? (
-              <img
-                className="mt-10"
-                src="/images/misc/powered-by-polygon.svg"
-                alt="Powered by Polygon"
-              />
-            ) : null}
-          </div>
+      <div className="mx-auto lg:w-[45%]">
+        <StraddleClosedBanner />
+        <div className="lg:max-w-4xl md:max-w-3xl sm:max-w-2xl max-w-md mx-auto px-4 lg:px-0">
+          <TopBar />
+        </div>
+        <div className="pt-5 lg:max-w-4xl md:max-w-3xl sm:max-w-3xl max-w-md mx-auto px-2 lg:px-0">
+          <Stats />
+        </div>
+        <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 mt-5">
+          <div className="-ml-1 text-sm">Deposits</div>
+        </div>
+        <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
+          <Deposits />
+        </div>
+        <div className="pt-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0">
+          <div className="-ml-1 text-sm">Positions</div>
+        </div>
+        <div className="mb-5 py-2 lg:max-w-4xl md:max-w-3xl md:m-0 mx-3 sm:max-w-3xl max-w-md lg:mx-auto px-2 lg:px-0 flex-auto">
+          <Positions />
+          {chainId === 137 ? (
+            <img
+              className="mt-10"
+              src="/images/misc/powered-by-polygon.svg"
+              alt="Powered by Polygon"
+            />
+          ) : null}
         </div>
       </div>
     </div>
