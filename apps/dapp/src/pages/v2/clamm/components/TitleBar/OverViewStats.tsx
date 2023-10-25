078dbd36d4afbc2b0e609c6e1a53ed6957959938
Aercwarden <arc@dopex.io>
2023-10-25 14:52:14 +0400

    feat: use query

--- apps/dapp/src/pages/v2/clamm/components/TitleBar/OverViewStats.tsx
+++ apps/dapp/src/pages/v2/clamm/components/TitleBar/OverViewStats.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
 
 import { Skeleton } from '@dopex-io/ui';
+import { useQuery } from '@tanstack/react-query';
+import { useNetwork } from 'wagmi';
 
+import useClammStore from 'hooks/clamm/useClammStore';
 import useLoadingStates from 'hooks/clamm/useLoadingStates';
 
+import { formatAmount } from 'utils/general';
+
+import { VARROACK_BASE_API_URL } from '../../constants';
+
 const OverViewStats = () => {
-  const { isLoading } = useLoadingStates();
+  const { chain } = useNetwork();
+  const { selectedOptionsPool } = useClammStore();
+
+  const { data, refetch, dataUpdatedAt, isLoading } = useQuery({
+    queryKey: ['clamm-pools-stats'],
+    initialData: {
+      tvl: 0,
+      openInterest: 0,
+      markPrice: 0,
+    },
+    queryFn: () => {
+      const queryUrl = new URL(`${VARROACK_BASE_API_URL}/clamm/stats`);
+      queryUrl.searchParams.set('chainId', chain?.id.toString() ?? '42161');
+      queryUrl.searchParams.set(
+        'callToken',
+        selectedOptionsPool?.callToken.symbol ?? '',
+      );
+      queryUrl.searchParams.set(
+        'putToken',
+        selectedOptionsPool?.putToken.symbol ?? '',
+      );
+      return fetch(queryUrl).then((res) => res.json());
+    },
+  });
+
   return (
     <div className="flex space-x-[24px] md:w-fit w-full justify-between md:justify-normal">
       <div className="flex flex-col">
         <h6 className="flex text-sm md:text-md font-medium text-white items-center space-x-2">
           <span className="text-stieglitz">$</span>
-          {isLoading('initial_pool_load') ? (
+          {isLoading ? (
             <Skeleton width={'24px'} height={'12px'} variant="rounded" />
           ) : (
-            <span>{(0).toFixed(5)}</span>
+            <span>{formatAmount(data.markPrice ?? 0, 5)}</span>
           )}
         </h6>
         <h6 className="text-sm md:text-md font-medium text-stieglitz">
@@ -24,10 +55,10 @@ const OverViewStats = () => {
       <div className="flex flex-col">
         <h6 className="flex text-sm md:text-md font-medium text-white items-center space-x-2">
           <span className="text-stieglitz">$</span>{' '}
-          {isLoading('initial_pool_load') ? (
+          {isLoading ? (
             <Skeleton width={'24px'} height={'12px'} variant="rounded" />
           ) : (
-            <span>{(0).toFixed(5)}</span>
+            <span>{formatAmount(data.openInterest ?? 0, 5)}</span>
           )}
         </h6>
         <h6 className="text-sm md:text-md font-medium text-stieglitz">
@@ -37,10 +68,10 @@ const OverViewStats = () => {
       <div className="flex flex-col">
         <h6 className="flex text-sm md:text-md font-medium text-white items-center space-x-2">
           <span className="text-stieglitz">$</span>{' '}
-          {isLoading('initial_pool_load') ? (
+          {isLoading ? (
             <Skeleton width={'24px'} height={'12px'} variant="rounded" />
           ) : (
-            <span>{(0).toFixed(5)}</span>
+            <span>{formatAmount(data.tvl ?? 0, 5)}</span>
           )}
         </h6>
         <h6 className="text-sm md:text-md font-medium text-stieglitz">
