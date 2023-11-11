87951596b1c9847e671e8f223a4053545e7ff6e7
WitherBlock <witherblock@protonmail.com>
2023-11-11 20:24:52 +0400

    chore: add side image

--- apps/dapp/src/pages/index.tsx
+++ apps/dapp/src/pages/index.tsx
@@ -15,24 +15,33 @@ const Home = () => {
       </Head>
       <AppBar />
       <div className="lg:max-w-5xl mx-auto lg:px-0 px-4 pb-28 pt-32">
-        <div className="mb-16 lg:mx-0 lg:max-w-xl mx-auto max-w-lg">
-          <div className="flex mb-4 items-center">
-            <img
-              src="/images/brand/logo.svg"
-              alt="logo"
-              className="md:w-14 md:h-14 w-12 h-12 md:mr-4 mr-2"
-            />
-            <h1 className="md:text-6xl text-5xl font-mono font-bold">DOPEX</h1>
+        <div className="flex items-start justify-between">
+          <div className="mb-16 lg:mx-0 lg:max-w-xl mx-auto max-w-lg">
+            <div className="flex mb-4 items-center">
+              <img
+                src="/images/brand/logo.svg"
+                alt="logo"
+                className="md:w-14 md:h-14 w-12 h-12 md:mr-4 mr-2"
+              />
+              <h1 className="md:text-6xl text-5xl font-mono font-bold">
+                DOPEX
+              </h1>
+            </div>
+            <div className="text-lg mb-4">
+              Arbitrum STIP Incentives are LIVE on Dopex now! Bond on rDPX V2 or
+              Deposit on CLAMM now to earn up to <b>182% APY!</b>
+            </div>
+            <div>
+              <Button>rDPX V2 Bond</Button>
+              <span className="text-stieglitz mx-4">or</span>
+              <Button>Deposit CLAMM</Button>
+            </div>
           </div>
-          <div className="text-lg mb-4">
-            Arbitrum STIP Incentives are LIVE on Dopex now! Bond on rDPX V2 or
-            Deposit on CLAMM now to earn up to <b>182% APY!</b>
-          </div>
-          <div>
-            <Button>rDPX V2 Bond</Button>
-            <span className="text-stieglitz mx-4">or</span>
-            <Button>Deposit CLAMM</Button>
-          </div>
+          <img
+            className="w-64 h-auto"
+            src="/images/brand/rdpx-v2.svg"
+            alt="rdpx-v2"
+          />
         </div>
         {/* <div className="flex md:flex-row md:space-y-0 space-y-12 flex-col justify-between mb-16 w-2/3">
           <div className="flex flex-col max-w-fit">
