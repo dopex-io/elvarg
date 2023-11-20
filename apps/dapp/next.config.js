5f0f57b01cad93457094c5396036c7424f6a938a
WitherBlock <witherblock@protonmail.com>
2023-11-20 10:21:44 +0400

    chore: cleanup next config

--- apps/dapp/next.config.js
+++ apps/dapp/next.config.js
@@ -7,21 +7,11 @@ const nextConfig = {
   sentry: {
     hideSourceMaps: true,
   },
-  generateBuildId: () => 'build',
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-    unoptimized: process.env.OPTIMIZE_IMAGES === 'false' ? true : false,
-  },
   async redirects() {
     return [
       {
         source: '/share',
         destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/ssov-v3/:path*',
-        destination: '/ssov/:path*',
         permanent: false,
       },
     ];
