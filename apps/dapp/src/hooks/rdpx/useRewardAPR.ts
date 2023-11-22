82d26eb5204cdae3677219121112b7446e05210c
WitherBlock <witherblock@protonmail.com>
2023-11-22 17:15:00 +0400

    refactor(rdpx-v2): apr hook

--- apps/dapp/src/hooks/rdpx/useRewardAPR.ts
+++ apps/dapp/src/hooks/rdpx/useRewardAPR.ts
@@ -3,7 +3,7 @@ import { formatUnits } from 'viem';
 
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useContractRead } from 'wagmi';
+import { useContractReads } from 'wagmi';
 
 import { formatAmount } from 'utils/general';
 
@@ -11,50 +11,61 @@ import CurveMultiRewards from 'constants/rdpx/abis/CurveMultiRewards';
 import addresses from 'constants/rdpx/addresses';
 
 const useRewardAPR = () => {
-  // const ethPriceQuery = useQuery({
-  //   queryKey: ['eth-price'],
-  //   queryFn: async () => {
-  //     return await axios.get('https://api.dopex.io/v2/price/eth');
-  //   },
-  // });
+  const priceQuery = useQuery({
+    queryKey: ['eth-price'],
+    queryFn: async () => {
+      const [eth, arb] = await Promise.all([
+        axios.get('https://api.dopex.io/v2/price/eth'),
+        axios.get('https://api.dopex.io/v2/price/arb'),
+      ]);
 
-  // console.log(ethPriceQuery);
-
-  const { data: totalSupplyperp } = useContractRead({
-    abi: CurveMultiRewards,
-    address: addresses.perpVaultStaking,
-    functionName: 'totalSupply',
+      return { eth: eth.data.cgPrice, arb: arb.data.cgPrice };
+    },
+    staleTime: 50000,
   });
 
-  const { data: totalSupplyrt } = useContractRead({
-    abi: CurveMultiRewards,
-    address: addresses.receiptTokenStaking,
-    functionName: 'totalSupply',
+  const data = useContractReads({
+    contracts: [
+      {
+        abi: CurveMultiRewards,
+        address: addresses.receiptTokenStaking,
+        functionName: 'totalSupply',
+      },
+      {
+        abi: CurveMultiRewards,
+        address: addresses.perpVaultStaking,
+        functionName: 'totalSupply',
+      },
+    ],
   });
 
   return useMemo(() => {
-    if (totalSupplyperp === undefined || totalSupplyrt === undefined) {
+    if (
+      data.data === undefined ||
+      data.data[0].result === undefined ||
+      data.data[1].result === undefined ||
+      priceQuery.data === undefined
+    ) {
       return {
         ppvRewardAPR: '0',
         rtRewardAPR: '0',
       };
     }
 
+    const perpVaultDailyRewardsInUSD = 3214 * Number(priceQuery.data.arb);
+    const receiptTokenDailyRewardsInUSD = 9624 * Number(priceQuery.data.arb);
+
     const [ppvRewardAPR, rtRewardAPR] = [
       formatAmount(
-        ((3214 * 365) /
-          (Number(formatUnits(1000000000000000000n, 18)) * 2020)) *
+        ((perpVaultDailyRewardsInUSD * 365) /
+          (Number(formatUnits(data.data[1].result, 18)) *
+            Number(priceQuery.data.eth))) *
           100,
       ),
       formatAmount(
-        ((9624 * 365) /
-          (Number(
-            formatUnits(
-              totalSupplyrt === 0n ? 1000000000000000000n : totalSupplyrt!,
-              18,
-            ),
-          ) *
-            2020)) *
+        ((receiptTokenDailyRewardsInUSD * 365) /
+          (Number(formatUnits(data.data[0].result, 18)) *
+            Number(priceQuery.data.eth))) *
           100,
       ),
     ];
@@ -63,7 +74,7 @@ const useRewardAPR = () => {
       ppvRewardAPR,
       rtRewardAPR,
     };
-  }, [totalSupplyperp, totalSupplyrt]);
+  }, [data.data, priceQuery.data]);
 };
 
 export default useRewardAPR;
