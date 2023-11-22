0d069833d21741a29e531951cad34802163dc920
WitherBlock <witherblock@protonmail.com>
2023-11-22 12:35:51 +0400

    chore: remove hardhat network

--- apps/dapp/src/wagmi-config/index.ts
+++ apps/dapp/src/wagmi-config/index.ts
@@ -1,5 +1,5 @@
 import { configureChains, createConfig } from 'wagmi';
-import { arbitrum, hardhat, mainnet, polygon } from 'wagmi/chains';
+import { arbitrum, mainnet, polygon } from 'wagmi/chains';
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { LedgerConnector } from 'wagmi/connectors/ledger';
@@ -14,7 +14,7 @@ import { OkxConnector } from './OkxConnector';
 import { RabbyConnector } from './RabbyConnector';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [arbitrum, polygon, mainnet, hardhat],
+  [arbitrum, polygon, mainnet],
   [infuraProvider({ apiKey: INFURA_PROJECT_ID || '' }), publicProvider()],
 );
 
