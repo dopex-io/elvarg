ff60c6fc80011814a2a8195ed16e7d83eed74e12
hibou-de-nuit <hp@dopex.io>
2023-11-23 07:20:50 +0400

    fix: subgraph url

--- apps/dapp/src/constants/subgraphs.ts
+++ apps/dapp/src/constants/subgraphs.ts
@@ -11,4 +11,4 @@ export const DOPEX_POLYGON_SSOV_SUBGRAPH_API_URL =
   'https://api.thegraph.com/subgraphs/name/garyunwin42/dopex-ssov-polygon';
 
 export const DOPEX_RDPX_V2_SUBGRAPH_API_URL =
-  'http://api.0xgraph.xyz/subgraphs/name/rdpx-v2-3';
+  'https://api.0xgraph.xyz/subgraphs/name/rdpx-v2-3';
