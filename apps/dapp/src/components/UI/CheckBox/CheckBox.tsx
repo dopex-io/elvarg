adbd4944643c748798d513ebcf4ff6056f2e685f
Aercwarden <arc@dopex.io>
2024-01-04 20:14:56 +0400

    chore: remove redundant code

--- apps/dapp/src/components/UI/CheckBox/CheckBox.tsx
+++ apps/dapp/src/components/UI/CheckBox/CheckBox.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { CheckIcon } from '@heroicons/react/24/solid';
 import {
   CheckboxIndicatorProps,
--- apps/dapp/src/components/clamm/PositionsTable/components/Positions/BuyPositions/components/MultiExerciseButton/index.tsx
+++ apps/dapp/src/components/clamm/PositionsTable/components/Positions/BuyPositions/components/MultiExerciseButton/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BaseError, encodeAbiParameters, encodeFunctionData } from 'viem';
 
 import { Button } from '@dopex-io/ui';
