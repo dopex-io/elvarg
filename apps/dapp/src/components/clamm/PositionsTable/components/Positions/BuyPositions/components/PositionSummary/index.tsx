dc3a9e2059fd47c87a1a9c734db331aa9a357e0b
Aercwarden <90272722+Aercwarden@users.noreply.github.com>
2024-01-05 18:01:56 +0400

    fix: positions summary decimals (#846)

--- apps/dapp/src/components/clamm/PositionsTable/components/Positions/BuyPositions/components/PositionSummary/index.tsx
+++ apps/dapp/src/components/clamm/PositionsTable/components/Positions/BuyPositions/components/PositionSummary/index.tsx
@@ -22,7 +22,7 @@ const PositionSummary = ({
         <span className="text-xs flex items-center justify-center space-x-[2px]">
           <span className="text-stieglitz">$</span>
           <span className={cn(totalProfitUsd > 0 && 'text-up-only')}>
-            {formatAmount(totalProfitUsd, 3)}
+            {formatAmount(totalProfitUsd, 5)}
           </span>
         </span>
       </div>
@@ -30,13 +30,13 @@ const PositionSummary = ({
         <span className="text-stieglitz text-xs">Total premium:</span>
         <span className="text-xs flex items-center justify-center space-x-[2px]">
           <span className="text-stieglitz">$</span>
-          <span>{formatAmount(totalPremiumUsd, 3)}</span>
+          <span>{formatAmount(totalPremiumUsd, 5)}</span>
         </span>
       </div>
       <div className="flex items-center justify-center space-x-[4px]">
         <span className="text-stieglitz text-xs">Total size:</span>
         <span className="text-xs flex items-center justify-center space-x-[4px]">
-          <span>{formatAmount(totalOptions, 3)}</span>
+          <span>{formatAmount(totalOptions, 5)}</span>
           <span className="text-stieglitz text-xs">{callTokenSymbol}</span>
         </span>
       </div>
