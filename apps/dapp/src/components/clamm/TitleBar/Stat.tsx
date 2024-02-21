3596083bd0c1d36b34edcc3a1f037017392564ba
Aercwarden <arc@dopex.io>
2024-02-21 21:16:45 +0400

    refactor

--- apps/dapp/src/components/clamm/TitleBar/Stat.tsx
+++ apps/dapp/src/components/clamm/TitleBar/Stat.tsx
@@ -15,14 +15,12 @@ const Stat = (props: Props) => {
   return (
     <div className="flex flex-col">
       <h6
-        className={`flex ${
-          suffix ? 'flex-row-reverse' : 'flex-row'
-        } text-xs sm:text-sm md:text-md font-medium text-white items-center space-x-1`}
+        className={`flex text-[11px] sm:text-sm md:text-md font-medium text-white items-center space-x-2`}
       >
         <span className="text-stieglitz">{stat.symbol}</span>
-        <span>{formatAmount(stat.value ?? 0, 5)}</span>
+        <span>{formatAmount(stat.value ?? 0, 3)}</span>
       </h6>
-      <h6 className="text-xs sm:text-sm md:text-md font-medium text-stieglitz">
+      <h6 className="text-[11px] sm:text-sm md:text-md font-medium text-stieglitz">
         {label}
       </h6>
     </div>
